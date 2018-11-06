1ffe7c0f464e99ef15b13b8a4342ffc29b44df8d
jeremy richard <jeremy.richard@sciencespo.fr>
2018-11-06 11:39:04 +0100

    Startup information 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 chown -R node:node /website
+echo "Starting hydrate:json..."
 [ ! -f data/people.json ] && npm run hydrate:json
 echo "Starting server..."
 su-exec node:node /usr/local/bin/npm start

aebecd9d0b74d91f3000eaee90c4f35fcc0687b6
jeremy richard <jeremy.richard@sciencespo.fr>
2018-11-06 12:42:46 +0100

    gatsby build at startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+echo "chown /website..."
 chown -R node:node /website
+echo "First gatsby build..."
+cd /website/site/ && su-exec node:node /website/site/node_modules/.bin/gatsby build
+cd -
 echo "Starting hydrate:json..."
 [ ! -f data/people.json ] && npm run hydrate:json
 echo "Starting server..."

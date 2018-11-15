1f1837138d2860bfc7078b06b42b961f74d5e5c5
Yomguithereal <guillaumeplique@gmail.com>
2018-11-15 12:25:13 +0100

    Summary on index for convenience

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
+import {Link} from 'gatsby';
 
 import Layout from '../components/layout';
 
-const IndexPage = () => (
-  <Layout>
-    <h1>médialab Static website</h1>
-    <p>Hello World!</p>
-  </Layout>
-);
+export const query = graphql`
+  {
+    allPeopleJson {
+      edges {
+        node {
+          id
+          firstName
+          lastName
+        }
+      }
+    }
+    allActivitiesJson {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+const IndexPage = ({data}) => {
+
+  const people = data.allPeopleJson.edges.map(e => e.node),
+        activities = data.allActivitiesJson.edges.map(e => e.node);
+
+  return (
+    <Layout>
+      <h1>médialab Static website</h1>
+      <h2>People</h2>
+      <ul>
+        {people.map(p => (
+          <li key={p.id}>
+            <Link to={`/people-${p.id}`}>{p.firstName} {p.lastName}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Activities</h2>
+      <ul>
+        {activities.map(a => (
+          <li key={a.id}>
+            <Link to={`/activity-${a.id}`}>{a.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+}
 
 export default IndexPage;

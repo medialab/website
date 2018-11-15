bd2c0e5ac090ec124572db228d07d59a57c86791
Yomguithereal <guillaumeplique@gmail.com>
2018-11-15 11:00:27 +0100

    Adding dump script Related to #33

---
+++ scripts/dump.js
@@ -0,0 +1,30 @@
+const stableJson = require('json-stable-stringify'),
+      config = require('config'),
+      path = require('path'),
+      fs = require('fs-extra');
+
+const models = require('../specs/models.json');
+
+fs.ensureDirSync('./dump');
+
+models.forEach(model => {
+  const p = path.join('./dump', `${model}.json`);
+
+  fs.ensureDirSync(path.join('./dump', model));
+
+  const raw = fs.readFileSync(path.join(config.get('data'), `${model}.json`), 'utf-8');
+
+  const data = JSON.parse(raw);
+
+  const list = data[model];
+
+  if (!list)
+    return;
+
+  list.forEach(item => {
+    fs.writeFileSync(
+      path.join('./dump', model, `${item.id}.json`),
+      stableJson(item, {space: 2})
+    );
+  });
+});

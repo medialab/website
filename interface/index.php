0091ae1c066e1a53237d35c7bdf3f6b1bd6b38f2
BenjmnG <Benjmng@gmail.com>
2018-11-15 13:22:28 +0100

    Proto_interface pour experimentation

---
+++ interface/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	
+    <title>Medialab - Proto</title>
+    <!-- styles -->
+    <style type="text/css"></style>
+    <link rel="stylesheet" href="assets/fonts/stylesheet.css">
+    <link rel='stylesheet' type='text/css' href='assets/css-compiled/theme.css'>
+
+    <!-- scripts -->
+
+
+</head>
+
+<body>
+	<div id="topbar">
+		<div class="logo">
+			<img src="assets/images/Medialab_Embleme_RVB.svg">
+		</div>
+		<div class="menu objet">
+			<p><a href="page_objet.php">Actualité</a></p>
+			<p><a href="page_objet.php">Publication</a></p>
+			<p><a href="page_objet.php">Activité</a></p>
+		</div>
+		<div class="menu humain">
+			<p><a href="page_objet.php">Le Médialab</a></p>
+			<p><a href="page_objet.php">L'équipe</a></p>
+		</div>
+		<div class="menu archive">
+			<p><a href="archive.php">Archive</a></p>
+		</div>
+
+		<div class="menu langue">
+			<p><span>FR</span><span>|</span><span>EN</span></p>
+		</div>
+	</div>
+
+	<div class="container">
+
+		<div class="introduction">
+			<p>Le Médialab est itinusdaepel mo tem. Quo cuscilis asi a deliqui.
+			</p>
+		</div>
+
+		<div class="actualites">
+
+			<div class="item">
+				<h2>Un titre d'actualité</h2>
+			</div>
+
+			<div class="item">
+				<h2>Un titre d'actualité</h2>
+			</div>
+
+			<div class="item">
+				<h2>Un titre d'actualité</h2>
+			</div>
+
+			<div class="item">
+				<h2>Un titre d'actualité</h2>
+			</div>
+		</div>
+
+		<div class="flux">
+			<p>flux </p>
+		</div>
+
+	</div>
+</body>
+</html>

1a9e73be1ab5b9c85dba2af6d7a4e910dc079153
JulieBlanc <contact@julie-blanc.fr>
2018-11-18 00:25:42 +0100

    structuration scss & début de prototype pour la page objet

---
+++ prototyping/page_objet_02.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	
+    <title>Medialab - Proto</title>
+
+
+    <!-- styles -->
+    <link rel='stylesheet' type='text/css' href='assets/css-compiled/page_objet_02.css'>
+
+    <!-- typos -->
+    <link rel="stylesheet" href="assets/fonts/Museo_Sans/stylesheet.css">
+
+
+  </head>
+
+  <body>
+
+      <?php include('fragments/_topbar_2.php'); ?>
+
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet, <a href="#">consectetur</a> adipiscing elit. Vestibulum convallis mauris sapien, eu rutrum magna accumsan ut. Etiam faucibus neque vitae urna accumsan, id ornare odio sagittis. Vestibulum et laoreet sapien. In turpis leo, faucibus a finibus ac, pellentesque sed metus. Suspendisse euismod nisl vel nibh varius, ac finibus neque scelerisque. Sed sit amet purus interdum, commodo odio vitae, ultricies felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in porta est, in sollicitudin tortor. In facilisis, nibh sed tincidunt pretium, nisi lacus pharetra nisi, id congue ante dui id quam.
+        </p>
+        <p>
+            Phasellus ultrices urna libero, eget lacinia urna finibus sed. Nulla mattis diam id imperdiet facilisis. Nam quam elit, finibus in nibh a, aliquam finibus velit. Mauris non bibendum tortor, at convallis libero. Aliquam tincidunt, erat vel fringilla eleifend, lorem nunc pretium nisi, id consectetur nibh felis vitae risus. Donec lobortis magna et scelerisque vulputate. Integer faucibus orci leo. Mauris quis justo nunc. Donec condimentum tellus nibh, sit amet tempus felis placerat non.
+        </p>
+        <p>
+            Integer luctus elit at pulvinar venenatis. Nam consequat elementum nunc dictum posuere. Vestibulum non magna lacinia metus consectetur convallis id sed diam. Praesent mattis quam in leo euismod, eget maximus sem tincidunt. Cras ante dui, congue et mauris sed, aliquam auctor risus. Nullam ex sem, rhoncus eu porta sit amet, auctor eget est. Maecenas mi massa, rutrum eu erat quis, aliquet ornare erat. Duis lobortis tortor enim, non tempus sapien commodo eget. Suspendisse potenti. Donec eget magna diam.
+        </p>
+        <p>
+            Sed iaculis eros at quam mollis, eu varius urna fringilla. In nec accumsan tellus, nec fringilla arcu. Donec sit amet felis eget tellus condimentum varius et et erat. Integer non dolor lacus. Donec et malesuada arcu. Ut id ornare diam, id elementum sapien. Proin non nisl libero. Proin nulla lectus, interdum vitae lobortis nec, facilisis eget eros. Vivamus cursus congue consectetur. Curabitur gravida risus erat, ullamcorper suscipit nunc condimentum eget. Nam pretium hendrerit ultrices.
+        </p>
+      </main>
+
+      <aside id="aside">
+
+          <section id="related-content">
+    
+              <input id="_1" type="radio" name="c1"> 
+              <label class="collapse" for="_1">Équipe</label>
+              
+              
+              
+              <input id="_2" type="radio" name="c1">
+              <label class="collapse" for="_2">Publications</label>
+    
+              
+              <input id="_3" type="radio" name="c1">
+              <label class="collapse" for="_3">Activités</label>
+              
+              <div class="content content1">
+                <h1>Membres qui ont participés au projet : </h1>
+    
+              </div>
+    
+              <div class="content content2">
+                  <h1>Publications liées au projet : </h1>
+                
+              </div>
+    
+              <div class="content content3">
+                  <h1>Activités liées au projet : </h1>
+              </div>
+              
+    
+          </section>
+        </aside>
+
+
+    
+
+
+  </body>
+  
+</html>

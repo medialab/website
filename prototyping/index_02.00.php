6556b54958ce56085e3c910718c42f66fc76d433
BenjmnG <Benjmng@gmail.com>
2018-11-20 18:22:17 +0100

    Working on Image

---
+++ prototyping/index_02.00.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	
+    <title>Medialab - Proto</title>
+    <!-- styles -->
+    <style type="text/css"></style>
+    <link rel="stylesheet" href="assets/fonts/Museo_Sans/stylesheet.css">
+    <link rel='stylesheet' type='text/css' href='assets/css-compiled/theme.css'>
+
+
+
+</head>
+
+<body>
+	<?php include('fragments/_topbar.html'); ?>
+
+	<div class="container">
+
+		<div class="introduction">
+			<p>Le Médialab est itinusdaepel mo tem. Quo cuscilis asi a deliqui.
+			</p>
+		</div>
+
+		<div class="actualites">
+
+			<div class="item-xxl" style="width: 48vw; height: 60vh; border: 1px solid grey; position: relative; overflow: hidden" >
+				<?php include('_bac_a_sable/module/ascii_2.9.html'); ?>
+			</div>
+
+			<div class="item-xxl" style="width: 48vw; height: 60vh; border: 1px solid grey; position: relative; overflow: hidden; grid-column: 2 / 4; grid-row: 1 / 2;"  >
+				<?php include('_bac_a_sable/module/ascii_2.11.2.html'); ?>
+			</div>			
+
+ 
+
+	</div>
+</body>
+</html>

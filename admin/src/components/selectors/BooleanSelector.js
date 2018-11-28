0899e53bc37f194690dca4a41fde7cb1f7ac658d
Robin de Mourat <robin.demourat@gmail.com>
2018-11-28 17:42:41 +0100

    allow specific labels for boolean selector

--- admin/src/components/selectors/BooleanSelector.js
+++ admin/src/components/selectors/BooleanSelector.js
@@ -2,16 +2,16 @@ import React from 'react';
 import cls from 'classnames';
 
 export default function BooleanSelector(props) {
-  const {value, onChange} = props;
+  const {value, onChange, labels = ['Yes', 'No']} = props;
 
   return (
     <div className="buttons has-addons">
       <span
         className={cls('button', value && ['is-success', 'is-selected'])}
-        onClick={() => onChange(true)}>Yes</span>
+        onClick={() => onChange(true)}>{labels[0]}</span>
       <span
         className={cls('button', !value && ['is-warning', 'is-selected'])}
-        onClick={() => onChange(false)}>No</span>
+        onClick={() => onChange(false)}>{labels[1]}</span>
     </div>
   );
 }

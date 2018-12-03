93907764053c3b7d9bd4a602d188eb376c3887f5
JulieBlanc <contact@julie-blanc.fr>
2018-12-03 12:04:15 +0100

    accroche fixe (publications seulement)

---
+++ prototyping/fragments/_liste_objet_4.php
@@ -0,0 +1,147 @@
+<section id="menu_objet">
+
+	<div id="filtre_objet">
+		<ul >
+			<li><p>A-Z</p></li>
+			<li><p>Type</p></li>
+			<li><p>Date</p></li>
+		</ul>
+	</div>
+
+	<div id="liste_objet">
+
+		<nav id="liste_actualite" >
+			<ul id="">
+				<h3>Actualités</h3>
+				<li data-item="1">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+						<p class="item_accroche" data-item-accroche="1">
+							Voici une phrase spécifique sur le projet 1.
+							
+						</p>
+					</a>
+				</li>
+				<li data-item-accroche="2">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+						<p class="item_accroche" data-item-accroche="2">
+							Voici une grande phrase spécifique sur le projet 2.
+							
+						</p>
+					</a>
+				</li>
+				<li data-item-accroche="3">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+						<p class="item_accroche" data-item-accroche="3">
+							Voici un mouton..
+							
+						</p>
+					</a>
+				</li>
+			</ul>
+		</nav>
+
+		<nav id="liste_publication" >
+			<ul id="">
+				<h3>Publications</h3>
+				<li data-item="1">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+					
+					</a>
+				</li>
+				<li data-item="2">
+					<a href="#">
+						<h1>Quelle est la question de recherche de ce projet en quelques mots ? </h1>
+						<p>Date — Type d'article</p>
+					</a>
+				</li>
+				<li data-item="3">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+					</a>
+				</li>
+				<li data-item="4">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+					</a>
+				</li>
+				<li data-item="5">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+
+					</a>
+				</li>
+				<li class="item_accroche accroche-titre">Description en une phrase de la catégorie publication.</li>
+				<li class="item_accroche" data-item-accroche="1">Voici une phrase spécifique sur le projet 1.</li>
+				<li class="item_accroche" data-item-accroche="2">Voici une phrase spécifique sur le projet 2.</li>
+				<li class="item_accroche" data-item-accroche="3">Voici une phrase spécifique sur le projet 3.</li>
+				<li class="item_accroche" data-item-accroche="4">Voici une phrase spécifique sur le projet 4.</li>
+				<li class="item_accroche" data-item-accroche="5">Voici une phrase spécifique sur le projet 5.</li>
+			</ul>
+		</nav>
+
+		
+		<nav id="liste_activite" >
+			<ul id="">
+				<h3>Activités</h3>
+				<li>
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+						<p class="item_accroche">
+							Voici une phrase spécifique sur le projet.
+							
+						</p>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+						<p class="item_accroche">
+							Voici une phrase spécifique sur le projet.
+							
+						</p>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+						<p class="item_accroche">
+							Voici une phrase spécifique sur le projet.
+							
+						</p>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+						<p class="item_accroche">
+							Voici une phrase spécifique sur le projet.
+							
+						</p>
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</div><!-- end liste_objet -->
+
+
+
+
+	<!-- <div id="intro_objet">
+		
+	</div> -->
+</section>

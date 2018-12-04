e3ebadeda2ea7d1ed2c4888c4fc05eb6d6b7abdd
BenjmnG <Benjmng@gmail.com>
2018-12-04 16:13:27 +0100

    Inegration de page 4bis -  Julie

--- prototyping/fragments/_flux_1.php
+++ prototyping/fragments/_flux_1.php
@@ -8,14 +8,14 @@
 					◍
 				</div>
 				<div class="text_action">
-					<p>tweet</p>
+					<p>Medialab tweet</p>
 				</div>
 				<div class="text_content">
-					<p>“knowledge democracy”, wed. Nov. 7th - 2pm at médialab’s seminar : <a href="">@Noortje-Marres</a> explains “[why] we need to recover the central role in public life of experimental facts: statements whose truth value is unstable » Registrations on…</p>
+					<p>“knowledge democracy”, wed. Nov. 7th - 2pm at médialab’s seminar : <a href="">@Noortje-Marres</a> explains “[why] we need to recover the central...</p>
 				</div>					
 			</li>
 
-			<li class="item XL">
+			<li class="item XL machinalement">
 				<div class="emblem">
 					⊡
 				</div>
@@ -26,19 +26,58 @@
 					⬚
 				</div>
 				<div class="text_action">
-					<p>tweet</p>
+					<p>Bruno commit</p>
+				</div>
+				<div class="text_target">
+					<p>on AIME</p>
 				</div>
 				<div class="text_content">
 				</div>					
 			</li>
-			<li class="item S"></li>
+			<li class="item XL machinalement">
+				<div class="emblem">
+					▩
+				</div>
+				<div class="emblem_action">
+					⌥
+				</div>
+				<div class="emblem_target">
+					⬚
+				</div>
+				<div class="text_action">
+					<p>Marius merge</p>
+				</div>
+				<div class="text_target">
+					<p>Draft on RICardo</p>
+				</div>
+				<div class="text_content">
+				</div>					
+			</li>
+			<li class="item S">
+				<div class="emblem">
+					◍
+				</div>
+				<div class="text_action">
+					<p>Medialab suit désormais Noortje Marres</p>
+				</div>				
+				<div class="emblem_target">
+					⬚
+				</div>
+			</li>
+			<li class="item S">
+				<div class="emblem">
+					⧊
+				</div>
+				<div class="text_action">
+					<p>Marius suit désormais TANT lab</p>
+				</div>				
+				<div class="emblem_target">
+					⁙
+				</div>
+			</li>
 			<li class="item S"></li>
 			<li class="item M"></li>
 			<li class="item XL"></li>
 		</ul>
 	</section>
 </div>
-
-
-
-<p><span class="date">20 min ago</span class="date"> ◍ <span class="name">Medialab</span class="name"><span> tweet : “knowledge democracy”, wed. Nov. 7th - 2pm at médialab’s seminar : @Noortje-Marres explains “[why] we need to recover the central role in public life of experimental facts: statements whose truth value is unstable » Registrations on… <span class="date">22 hours ago</span class="date"> <span class="name">◉ Bruno</span class="name"><span>pushed</span> to master at medialab/website <span class="date">14 oct.</span class="date"> ◍ <span class="name">Medialab</span class="name"> <span>a Retweeté</span> : @Optichiasm #ModesOfExistence + #ResetModernity: A design laboratory for the Humanities se-lected by the @ADIassodesign Permanent Observatory for the Design Index 2018. Thanks to @BrunoLatourAIME & @medi-alab_ScPo for having made this possible! <span class="date">13 oct.</span class="date"> ◉ <span class="name">Alfred</span class="name"> <span>commit to</span> to master at medialab/website</p>
---
+++ prototyping/page_objet_04bis.php
@@ -0,0 +1,395 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	
+    <title>Medialab - Proto</title>
+    <!-- styles -->
+    <style type="text/css"></style>
+    <link rel="stylesheet" href="assets/fonts/Museo_Sans/stylesheet.css">
+    <link rel='stylesheet' type='text/css' href='assets/css-compiled/page_objet_04bis.css'>
+
+
+
+</head>
+
+<body>
+	<?php include('fragments/_topbar_4bis.php'); ?>
+
+
+
+  <section>
+
+<div id="nav-item-menu" class="nav-item-bottom">
+  <p><a href="#topbar"><?php include('assets/images/Medialab_Embleme_RVB.svg'); ?></a></p>
+</div>
+<div id="nav-item-bottom-1" class="nav-item-bottom">
+	<a href="#item-1"><p class="item-1">Publication</p></a>
+</div>
+<div id="nav-item-bottom-2" class="nav-item-bottom">
+  <a href="#item-2"><p class="item-2">Fichiers associés</p></a>
+</div>
+<div id="nav-item-bottom-3" class="nav-item-bottom">
+	<a href="#item-3"><p class="item-3">Personnes liées</p></a>
+</div>
+<div id="nav-item-bottom-4" class="nav-item-bottom">
+	<a href="#item-4"><p class="item-4">Publications liées</p></a>
+</div>
+
+
+<!-- PUBLICATION -->
+<article class="container" id="item-1">
+
+  <div class="sticky-nav"><a href="#item-1"><p class="item-1">Publiation</p></a></div>
+  <p class="titre-sticky">RICardo Project</p>
+  <div class="block">
+	  <div class=" block-content">
+		
+		  
+		<article><!-- supprimer cette balise, voir CSS -->
+
+
+		<main id="objet_majeur">
+			<hgroup>
+				<h1>Exploring XIX Century International Trade.</h1>
+				<h2>We present the RICardo data visualization application designed to explore a XIXth century international trade statistics database.</h2>
+
+				<p class="date">11/11/2016</p>
+				<p class="type">Conference</p>
+				
+			</hgroup>
+
+			<section>
+				<p>
+				That’s why many people are seeing a promising alternative in Compressed Air Energy Storage (CAES), another form of mechanical energy storage. In these systems, electricity is used to compress air, which is stored in an underground cavern. To make use of the stored energy, the air is decompressed and converted back to electricity.
+
+				Although CAES also requires favourable geography to provide the underground air storage caverns, it is believed that there are many more suitable sites worldwide than for pumped hydropower Energy storage. 3
+				</p>
+				
+				<h3>Lifetime</h3>
+
+				<p>If the energy stored over the lifetime of a storage device is compared to the amount of primary energy required to build the device, CAES is vastly superior to electrochemical batteries.
+				</p>
+
+				<img src="pessoa.jpg" alt="" />
+
+				<p>Importantly, CAES is the most sustainable energy storage around. Unlike pumped hydropower energy storage, compressed air energy storage presents no environmental issues caused by the flooding of land and the damming of rivers.
+
+				Furthermore, if the energy stored over the lifetime of a storage device is compared to the amount of primary energy required to build the device, CAES surpasses pumped hydropower energy storage and is vastly superior to electrochemical batteries, which require 10 to 100 times more embodied energy for a given storage capacity. 3
+
+				This is a crucial advantage, because high energy use for the production of the energy storage can greatly decrease the sustainability of a renewable power grid
+				The Problem with Compressed Air
+
+				In spite of these advantages, there are currently only two large-scale CAES plants in operation worldwide: one in Germany, built in 1979, and one in the USA, built in 1991. 4 This limited uptake is mainly attributed to the fact that more than half of the energy is lost when charging and discharging a compressed air “battery”.
+				</p>
+				
+				<h3>Delay</h3>
+
+				<p>
+				While pumped hydropower storage has a charge/discharge efficiency of 70-85%, and chemical batteries reach 65-90%, the CAES plants in operation in Germany and the US have an electric-to-electric efficiency of only 40-42% and 51-54%, respectively. 2 5 6
+
+				The low energy conversion efficiency is mainly due to the fact that air increases in temperature when being compressed to high pressures (both CAES plants operate at 50-70 bar, which is 10 to 20 times the air pressure in a bicycle tyre). Because the energy density of air decreases with rising temperature, both CAES plants remove the heat prior to storage and dump it into the atmosphere. This implies a significant source of energy loss.That’s why many people are seeing a promising alternative in Compressed Air Energy Storage (CAES), another form of mechanical energy storage. In these systems, electricity is used to compress air, which is stored in an underground cavern. To make use of the stored energy, the air is decompressed and converted back to electricity.
+
+				Although CAES also requires favourable geography to provide the underground air storage caverns, it is believed that there are many more suitable sites worldwide than for pumped hydropower Energy storage. 3
+
+				If the energy stored over the lifetime of a storage device is compared to the amount of primary energy required to build the device, CAES is vastly superior to electrochemical batteries
+
+				Importantly, CAES is the most sustainable energy storage around. Unlike pumped hydropower energy storage, compressed air energy storage presents no environmental issues caused by the flooding of land and the damming of rivers.
+
+				Furthermore, if the energy stored over the lifetime of a storage device is compared to the amount of primary energy required to build the device, CAES surpasses pumped hydropower energy storage and is vastly superior to electrochemical batteries, which require 10 to 100 times more embodied energy for a given storage capacity. 3
+
+				This is a crucial advantage, because high energy use for the production of the energy storage can greatly decrease the sustainability of a renewable power grid
+				The Problem with Compressed Air
+
+				In spite of these advantages, there are currently only two large-scale CAES plants in operation worldwide: one in Germany, built in 1979, and one in the USA, built in 1991. 4 This limited uptake is mainly attributed to the fact that more than half of the energy is lost when charging and discharging a compressed air “battery”.
+
+				While pumped hydropower storage has a charge/discharge efficiency of 70-85%, and chemical batteries reach 65-90%, the CAES plants in operation in Germany and the US have an electric-to-electric efficiency of only 40-42% and 51-54%, respectively. 2 5 6
+
+				The low energy conversion efficiency is mainly due to the fact that air increases in temperature when being compressed to high pressures (both CAES plants operate at 50-70 bar, which is 10 to 20 times the air pressure in a bicycle tyre). Because the energy density of air decreases with rising temperature, both CAES plants remove the heat prior to storage and dump it into the atmosphere. This implies a significant source of energy loss.</p>				</p>
+			</section>
+
+
+		</main>
+
+		</article>
+
+	  </div>
+  </div>
+
+  <div id="cache-2"><p></p></div>
+
+</article>
+
+
+<!-- FICHIERS ASSOCIÉS -->
+<aside class="container" id="item-2">
+
+  <nav>
+	<a href="#item-1"><p class="item-1">Publiation</p></a>
+  </nav>
+
+  <div class="sticky-nav"><a href="#item-2"><p class="item-2">Fichiers associés</p></a></div>
+  <p class="titre-sticky">Fichiers associés</p>
+
+  <div class="block">
+	<div class=" block-content">
+		
+
+		<ul>
+			<li>liste de </li>
+			<li>quelques fichiers</li>
+			<li>associés</li>
+			<li>et liens</li>
+		</ul>
+	</div>
+  </div>
+
+  <div id="cache-3"><p></p></div>
+
+</aside>
+
+
+<!-- PERSONNES LIÉES -->
+<aside class="container" id="item-3">
+
+  <nav>
+	<a href="#item-1"><p class="item-1">Publiation</p></a>
+	<a href="#item-2Ò"><p class="item-2">Fichiers associés</p></a>
+  </nav>
+
+  <div class="sticky-nav"><a href="#item-2"><p class="item-2">Personnes liées</p></a></div>
+  <p class="titre-sticky">Personnes liées</p>
+
+  <div class="block">
+	<div class=" block-content">
+	<ul>
+		<li>
+			<img src="pessoa.jpg" alt="">
+			<h1>Paul Girard</h1>
+			<p>Ingénieur, Directeur Technique</p>
+		</li>
+
+		<li>
+			<img src="pessoa.jpg" alt="">
+			<h1>Donato Ricci</h1>
+			<p>Designer Principal</p>
+		</li>
+
+		<li>
+		<img src="pessoa.jpg" alt="">
+			<h1>Benjamin Ooghe Tabanou</h1>
+			<p>Ingénieur de recherche</p>
+		</li>
+
+
+	</ul>
+	
+	</div>
+  </div>
+
+<p class="titre-sticky">Autres personnes du médialab</p>
+<div class="block">
+	<div class=" block-content other-random">
+	<ul>
+		<li>
+			<img src="pessoa.jpg" alt="">
+			<h1>Paul Girard</h1>
+			<p>Ingénieur, Directeur Technique</p>
+		</li>
+
+		<li>
+			<img src="pessoa.jpg" alt="">
+			<h1>Donato Ricci</h1>
+			<p>Designer Principal</p>
+		</li>
+
+		<li>
+		<img src="pessoa.jpg" alt="">
+			<h1>Benjamin Ooghe Tabanou</h1>
+			<p>Ingénieur de recherche</p>
+		</li>
+
+		<li>
+		<img src="pessoa.jpg" alt="">
+			<h1>Guillaume Plique</h1>
+			<p>Développeur</p>
+		</li>
+
+		<li>
+		<img src="pessoa.jpg" alt="">
+			<h1>Amélie Vairelles</h1>
+			<p>Communication Officer</p>
+		</li>
+		<li>
+		
+		<img src="pessoa.jpg" alt="">
+			<h1>Robin de Mourat</h1>
+			<p>Développeur &amp; designer d'interfaces</p>
+		</li>
+
+		<li>
+			<img src="pessoa.jpg" alt="">
+			<h1>Paul Girard</h1>
+			<p>Ingénieur, Directeur Technique</p>
+		</li>
+
+		<li>
+			<img src="pessoa.jpg" alt="">
+			<h1>Donato Ricci</h1>
+			<p>Designer Principal</p>
+		</li>
+
+		<li>
+		<img src="pessoa.jpg" alt="">
+			<h1>Guillaume Plique</h1>
+			<p>Développeur</p>
+		</li>
+
+		<li>
+		<img src="pessoa.jpg" alt="">
+			<h1>Amélie Vairelles</h1>
+			<p>Communication Officer</p>
+		</li>
+		<li>
+		
+		<img src="pessoa.jpg" alt="">
+			<h1>Robin de Mourat</h1>
+			<p>Développeur &amp; designer d'interfaces</p>
+		</li>
+
+		<li>
+			<img src="pessoa.jpg" alt="">
+			<h1>Paul Girard</h1>
+			<p>Ingénieur, Directeur Technique</p>
+		</li>
+
+	
+
+	</ul>
+	
+	</div>
+	</div>
+	
+	<div id="cache-4"><p></p></div>
+
+</aside>
+
+
+<!-- PUBLICATIONS LIÉES -->
+<aside class="container" id="item-4">
+
+  <nav>
+	  <a href="#item-1"><p class="item-1">Publiation</p></a>
+	  <a href="#item-2"><p class="item-2">Fichiers associés</p></a>
+	  <a href="#item-3"><p class="item-3">Publications associées</p></a>
+  </nav>
+
+  <div class="sticky-nav"><a href="#item-3"><p class="item-3">Publications associées</p></a></div>
+  <p class="titre-sticky">Publications liées</p>
+
+	<div class="block">
+	  <div class=" block-content">
+		<p>Il n'y a pas d'autres publications liées.</p>
+	  </div>
+  </div>
+
+
+  <p class="titre-sticky">Autres publications du médialab</p>
+	<div class="block">
+		<div class=" block-content other-random">
+		<ul>
+			<li>
+				<a href="#">
+					<p class="titre">NATURPRADI</p>
+					<h1>Le numérique change-t-il les formes de nature urbaine ?</h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+				<p class="titre">SHINRAI</p>
+					<h1>Quelle est la question de recherche de ce projet en quelques mots ? </h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<p class="titre">La fabrique de la loi</p>
+					<h1>Les parlementaires font-ils la loi ?</h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<p class="titre">NATURPRADI</p>
+					<h1>Le numérique change-t-il les formes de nature urbaine ?</h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+				<p class="titre">SHINRAI</p>
+					<h1>Quelle est la question de recherche de ce projet en quelques mots ? </h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<p class="titre">La fabrique de la loi</p>
+					<h1>Les parlementaires font-ils la loi ?</h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<p class="titre">NATURPRADI</p>
+					<h1>Le numérique change-t-il les formes de nature urbaine ?</h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+				<p class="titre">SHINRAI</p>
+					<h1>Quelle est la question de recherche de ce projet en quelques mots ? </h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<p class="titre">La fabrique de la loi</p>
+					<h1>Les parlementaires font-ils la loi ?</h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<p class="titre">NATURPRADI</p>
+					<h1>Le numérique change-t-il les formes de nature urbaine ?</h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+				<p class="titre">SHINRAI</p>
+					<h1>Quelle est la question de recherche de ce projet en quelques mots ? </h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<p class="titre">La fabrique de la loi</p>
+					<h1>Les parlementaires font-ils la loi ?</h1>
+					<p>Type publication – 01/01/2018</p>
+				</a>
+			</li>
+		</ul>
+		</div>
+	</div>
+
+</aside>
+		
+	
+</body>
+</html>

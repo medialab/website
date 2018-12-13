5ec58c405c2c5c9c9cee5f034721869696444aeb
Yomguithereal <guillaumeplique@gmail.com>
2018-12-13 11:21:03 +0100

    Moving utils to separate file

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,10 +1,15 @@
 /* eslint no-console: 0 */
 const fs = require('fs');
-const crypto = require('crypto');
 const path = require('path');
 const chokidar = require('chokidar');
 const GraphQLTypes = require('gatsby/graphql');
 const _ = require('lodash');
+
+const {
+  hashNode,
+  replaceHTMLAssetPaths,
+  createI18nPage
+} = require('./utils.js');
 
 const ROOT_PATH = process.env.ROOT_PATH || '..';
 
@@ -22,60 +27,6 @@ MODELS.forEach(model => {
 });
 
 MODELS_PATHS.settings = path.join(DB_PATH, 'settings.json');
-
-// Helper hashing a node's data
-function hashNode(data) {
-
-  return crypto
-    .createHash('md5')
-    .update(JSON.stringify(data))
-    .digest('hex');
-}
-
-// Helper replacing HTML assets
-function replaceHTMLAssetPaths(html, index) {
-
-  // TODO: this approach may be too slow in the future!
-  for (const base in index) {
-    const publicURL = index[base].publicURL;
-    html = html.replace(base, publicURL);
-  }
-
-  return html;
-}
-
-// Helper creating an internationalized page
-function createI18nPage(createPage, page) {
-
-  // Default page
-  createPage({
-    ...page,
-    context: {
-      ...page.context,
-      lang: 'fr'
-    }
-  });
-
-  // French page
-  createPage({
-    ...page,
-    path: '/fr' + page.path,
-    context: {
-      ...page.context,
-      lang: 'fr'
-    }
-  });
-
-  // English page
-  createPage({
-    ...page,
-    path: '/en' + page.path,
-    context: {
-      ...page.context,
-      lang: 'en'
-    }
-  });
-}
 
 const MODEL_READERS = {
   activities(createNode, deleteNode, getNode) {

dd162197160ae9ce90014e10375261346c4ab4d0
Yomguithereal <guillaumeplique@gmail.com>
2018-12-13 11:28:48 +0100

    Moving graphql schema initializers to own file

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -2,8 +2,12 @@
 const fs = require('fs');
 const path = require('path');
 const chokidar = require('chokidar');
-const GraphQLTypes = require('gatsby/graphql');
 const _ = require('lodash');
+
+const {
+  graphQLSchemaAdditionForSettings,
+  graphQLSchemaAdditionFromJsonSchema
+} = require('./schema.js');
 
 const {
   hashNode,
@@ -302,73 +306,10 @@ exports.createPages = function({graphql, actions}) {
   }).then(() => Promise.all(promises()));
 };
 
-function recurseIntoSchema(model, meta) {
-
-  if (meta.type === 'string')
-    return {type: GraphQLTypes.GraphQLString};
-
-  if (meta.type === 'number')
-    return {type: GraphQLTypes.GraphQLFloat};
-
-  if (meta.type === 'boolean')
-    return {type: GraphQLTypes.GraphQLBoolean};
-
-  // if (meta.type === 'array')
-  //   return {type: new GraphQLTypes.GraphQLList(GraphQLTypes.GraphQLString)};
-
-  if (meta.type === 'object') {
-    const fields = {};
-
-    for (const k in meta.properties)
-      fields[k] = recurseIntoSchema(model, meta.properties[k]);
-
-    return {
-      type: new GraphQLTypes.GraphQLObjectType({
-        name: model + '__' + _.deburr(meta.title),
-        fields
-      })
-    };
-  }
-}
-
-function graphQLSchemaAdditionFromJsonSchema(model, schema) {
-  const item = {};
-
-  for (const k in schema.properties) {
-    if (k === 'id')
-      continue;
-
-    const meta = schema.properties[k];
-    const addition = recurseIntoSchema(model, meta);
-
-    if (addition)
-      item[k] = addition;
-  }
-
-  return item;
-}
-
-function getSettingsSchema() {
-  return {
-    home: {
-      type: new GraphQLTypes.GraphQLObjectType({
-        name: 'settings__home',
-        fields: {
-          editorialization: {
-            type: new GraphQLTypes.GraphQLList(
-              new GraphQLTypes.GraphQLList(GraphQLTypes.GraphQLString)
-            )
-          }
-        }
-      })
-    }
-  };
-}
-
 exports.setFieldsOnGraphQLNodeType = function({type}) {
 
   if (type.name === 'SettingsJson') {
-    return getSettingsSchema();
+    return graphQLSchemaAdditionForSettings();
   }
 
   else if (type.name === 'ActivitiesJson') {

be02491ee66cd4736443a943e6b789491949652e
JulieBlanc <contact@julie-blanc.fr>
2018-12-13 22:47:15 +0100

    pages listing Julie (liées au menu)

---
+++ prototyping/fragments/_liste_objet_listing.php
@@ -0,0 +1,157 @@
+<section id="menu_objet">
+
+	<div id="filtre_objet">
+		<ul >
+			<li><p>A-Z</p></li>
+			<li><p>Type</p></li>
+			<li><p>Date</p></li>
+		</ul>
+	</div>
+
+	<div id="liste_objet">
+
+		<nav id="liste_actualite" >
+			<ul id="">
+				<h3>Actualités</h3>
+				<li data-item="1">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+						<p class="item_accroche" data-item-accroche="1">
+							Voici une phrase spécifique sur le projet 1.
+							
+						</p>
+					</a>
+				</li>
+				<li data-item-accroche="2">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+						<p class="item_accroche" data-item-accroche="2">
+							Voici une grande phrase spécifique sur le projet 2.
+							
+						</p>
+					</a>
+				</li>
+				<li data-item-accroche="3">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+						<p class="item_accroche" data-item-accroche="3">
+							Voici un mouton..
+							
+						</p>
+					</a>
+				</li>
+			</ul>
+		</nav>
+
+		<nav id="liste_publication" >
+			<ul id="">
+				<h3>Publications</h3>
+				<li data-item="1">
+					<a href="#">
+						<h1>Le numérique change-t-il les formes de nature urbaine ?</h1>
+						<ul>
+							<li class="titre">NATURPRADI</li>
+							<li>Type publication</li>
+							<li>01/01/2018</li>
+						</ul>
+					</a>
+				</li>
+				<li class="item_accroche" data-item-accroche="1">Nature(s) urbaine(s) en pratique(s) digitale(s)- étudie le retour de la nature en ville au prisme de ses effets sociaux, politiques et technologiques notamment grâce aux nouvelles technologies et dispositifs participatifs.</li>
+				<li data-item="2">
+					<a href="#">
+						<h1>Quelle est la question de recherche de ce projet en quelques mots ? </h1>
+						<ul>
+							<li>SHINRAI</li>
+							<li>Type publication</li>
+							<li>01/01/2018</li>
+						</ul>
+					</a>
+				</li>
+				<li data-item="3">
+					<a href="#">
+						<h1>Les parlementaires font-ils la loi ?</h1>
+						<ul>
+							<li>La fabrique de la loi</li>
+							<li>Type publication</li>
+							<li>01/01/2018</li>
+						</ul>
+					</a>
+				</li>
+				<li data-item="4">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+					</a>
+				</li>
+				<li data-item="5">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+
+					</a>
+				</li>
+				<li class="item_accroche accroche-titre">Description en une phrase de la catégorie publication.</li>
+				
+				<li class="item_accroche" data-item-accroche="2">La catastrophe de Fukushima a provoqué une crise politique et sociétale majeure au Japon. SHINRAI analyse la gestion faite par les autorités japonaises et sa réception par les populations.</li>
+				<li class="item_accroche" data-item-accroche="3">Ouvrir la boîte noire de la procédure législative en France grâce à l’exploration des données parlementaires. A destination des chercheurs, ce dispositif est également accessible à tous les citoyens et démocratise la compréhension des différentes étapes de fabrique de la loi.</li>
+				<li class="item_accroche" data-item-accroche="4">Voici une phrase spécifique sur le projet 4.</li>
+				<li class="item_accroche" data-item-accroche="5">Voici une phrase spécifique sur le projet 5.</li>
+			</ul>
+		</nav>
+
+		
+		<nav id="liste_activite" >
+			<ul id="">
+				<h3>Activités</h3>
+				<li data-item="1">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+					
+					</a>
+				</li>
+				<li data-item="2">
+					<a href="#">
+						<h1>Quelle est la question de recherche de ce projet en quelques mots ? </h1>
+						<p>Date — Type d'article</p>
+					</a>
+				</li>
+				<li data-item="3">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+					</a>
+				</li>
+				<li data-item="4">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+					</a>
+				</li>
+				<li data-item="5">
+					<a href="#">
+						<h1>Titre</h1>
+						<p>Date — Type d'article</p>
+
+					</a>
+				</li>
+				<li class="item_accroche accroche-titre">Description en une phrase de la catégorie publication.</li>
+				<li class="item_accroche" data-item-accroche="1">Voici une phrase spécifique sur le projet 1.</li>
+				<li class="item_accroche" data-item-accroche="2">Voici une phrase spécifique sur le projet 2.</li>
+				<li class="item_accroche" data-item-accroche="3">Voici une phrase spécifique sur le projet 3.</li>
+				<li class="item_accroche" data-item-accroche="4">Voici une phrase spécifique sur le projet 4.</li>
+				<li class="item_accroche" data-item-accroche="5">Voici une phrase spécifique sur le projet 5.</li>
+			</ul>
+		</nav>
+	</div><!-- end liste_objet -->
+
+
+
+
+	<!-- <div id="intro_objet">
+		
+	</div> -->
+</section>
---
+++ prototyping/fragments/_topbar_4.php
@@ -0,0 +1,54 @@
+<header id="topbar">
+	<div id="topbar-content">
+		<div id="logo-medialab">
+			<a href="#">
+				<?php include('../assets/images/Medialab_Embleme_RVB.svg'); ?>
+			</a>
+		</div>
+		<nav id="nav-option">
+			<ul id="nav-objet">
+				<!--
+				<li id="actualites">
+					<a href="#">Actualités</a>
+				</li>
+				<li id="publications" >
+					<a href="#">Publications</a>
+				</li>
+				<li id="activites">
+					<a href="#">Activités</a>
+				</li>
+			-->
+					<input id="actualites" type="radio" name="n1"> 
+	              	<label class="collapse" for="actualites">Actualités</label>
+
+					<input id="publications" type="radio" name="n1"> 
+	              	<label class="collapse" for="publications">Publications</label>
+
+					<input id="activites" type="radio" name="n1"> 
+	              	<label class="collapse" for="activites">Activités</label>
+
+					<input id="clear" type="radio" name="n1"> 
+	              	<label class="collapse" for="clear">✖ Fermer ✖</label>
+
+					<input id="clearzone" type="radio" name="n1"> 
+	              	<label id="clearzone_label" class="collapse" for="clearzone">✖</label>
+
+				<?php include('fragments/_liste_objet_listing.php'); ?>
+
+			</ul>
+			<ul id="nav-institution">
+				<li><a href="#">Le Médialab</a></li>
+				<li><a href="#">L'équipe</a></li>
+			</ul>
+			<ul id="nav-archive">
+				<li><a href="#">Archives</a></li>
+			</ul>
+
+
+		</nav>
+
+		<div id="langue" class="menu langue">
+			<p><span>FR</span><span>|</span><span>EN</span></p>
+		</div>
+	</div>
+</header>

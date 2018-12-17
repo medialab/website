5fe57b44933a2da89de537d5a7055236e2275bb0
Yomguithereal <guillaumeplique@gmail.com>
2018-12-17 12:29:02 +0100

    Adding capabilities to editor

--- admin/src/components/Editor.js
+++ admin/src/components/Editor.js
@@ -26,7 +26,14 @@ export default class Editor extends PureComponent {
             IMAGE
           ]}
           blockTypes={[
-            {type: BLOCK_TYPE.HEADER_ONE, label: 'H1'}
+            {type: BLOCK_TYPE.HEADER_ONE, label: 'H1'},
+            {type: BLOCK_TYPE.HEADER_TWO, label: 'H2'},
+            {type: BLOCK_TYPE.HEADER_THREE, label: 'H3'},
+            {type: BLOCK_TYPE.HEADER_FOUR, label: 'H4'},
+            {type: BLOCK_TYPE.HEADER_FIVE, label: 'H5'},
+            {type: BLOCK_TYPE.HEADER_SIX, label: 'H6'},
+            {type: BLOCK_TYPE.UNORDERED_LIST_ITEM, label: 'ul'},
+            {type: BLOCK_TYPE.ORDERED_LIST_ITEM, label: 'ol'}
           ]}
           inlineStyles={[
             {type: INLINE_STYLE.ITALIC, label: 'I'},

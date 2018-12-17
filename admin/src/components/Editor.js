22ed73a3ae8ec1f49212ca44e4c0b8ad459ea0f8
Yomguithereal <guillaumeplique@gmail.com>
2018-12-17 18:18:31 +0100

    Editor placeholder

--- admin/src/components/Editor.js
+++ admin/src/components/Editor.js
@@ -41,6 +41,7 @@ export default class Editor extends PureComponent {
     return (
       <div className="content">
         <DraftailEditor
+          placeholder='Type something...'
           rawContentState={content ? htmlToRaw(content) : null}
           stripPastedStyles={false}
           onSave={this.handleOnSave}

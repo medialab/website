af55b02794f2bd03f7728525a6897acbf9876818
Yomguithereal <guillaumeplique@gmail.com>
2018-12-17 17:03:48 +0100

    Better file naming in generation

--- site/src/templates/index.js
+++ site/src/templates/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link, graphql} from 'gatsby';
 
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 
 // NOTE: Helmet can be used in each page!
 

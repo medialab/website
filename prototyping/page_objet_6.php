f572afd7f6759066747beb65c486d27633f45144
JulieBlanc <contact@julie-blanc.fr>
2018-12-19 11:55:56 +0100

    page objet v6

---
+++ prototyping/page_objet_6.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	
+    <title>Medialab - Proto</title>
+    <!-- styles -->
+    <style type="text/css"></style>
+    <link rel="stylesheet" href="assets/fonts/Museo_Sans/stylesheet.css">
+    <link rel='stylesheet' type='text/css' href='assets/css-compiled/page_objet_6.css'>
+
+
+
+</head>
+
+<body>
+<?php include('fragments/_topbar_5.1.php'); ?>
+
+
+
+<nav id="nav-inside-article">
+	<div class="nav-inside-item">
+	<a href="#topbar">
+		<!-- <img src="assets/images/Medialab_logo_black_RVB.svg"> -->
+		<?php include('assets/images/Medialab_logo_black_RVB.svg'); ?>
+	</a>		
+	</div>
+	<div class="nav-inside-item" id="img-article">
+		<div>
+			<img src="assets/images/sample/RICardo_2017_2016_versions_b.png" alt="">
+			<div class="image-generator">
+				<?php include('assets/images/sample_txt/ricardo.html'); ?>
+			</div>
+
+		</div>
+	</div>
+	<div class="nav-inside-item" >
+		<a href="#item-1"><p class="item-1">Article</p></a>
+	</div>
+	<div class="nav-inside-item" >
+	<a href="#fichiers-associes"><p>Fichiers associés</p></a>
+	</div>
+	<div class="nav-inside-item">
+		<a href="#elements-associes"><p>Éléments liées</p></a>
+	</div>
+</nav>
+
+
+<main>
+
+	<p class="titre-sticky">RICardo Project</p>
+	<!-- PUBLICATION -->
+	<article id="item-1">
+
+		<hgroup>
+			<h1>Exploring XIX Century International Trade</h1>
+			<h2>We present the RICardo data visualization application designed to explore a XIXth century international trade statistics database.</h2>
+
+			<p class="date">11/11/2016</p>
+			<p class="type">Conference</p>
+			
+		</hgroup>
+
+		<div class="article-contenu">
+
+			<p>
+			That’s why many people are seeing a promising alternative in Compressed Air Energy Storage (CAES), another form of mechanical energy storage. In these systems, electricity is used to compress air, which is stored in an underground cavern. To make use of the stored energy, the air is decompressed and converted back to electricity.
+
+			Although CAES also requires favourable geography to provide the underground air storage caverns, it is believed that there are many more suitable sites worldwide than for pumped hydropower Energy storage. 3
+			</p>
+			
+			<h3>Lifetime</h3>
+
+			<p>If the energy stored over the lifetime of a storage device is compared to the amount of primary energy required to build the device, CAES is vastly superior to electrochemical batteries.
+			</p>
+
+			<figure>
+
+			<img src="assets/images/sample/RICardo_2017_2016_versions_b.png" alt="">
+			<figcaption>Ceci est une légende pour l'image.</figcaption>
+			</figure>
+
+			<p>Importantly, CAES is the most sustainable energy storage around. Unlike pumped hydropower energy storage, compressed air energy storage presents no environmental issues caused by the flooding of land and the damming of rivers.
+
+			Furthermore, if the energy stored over the lifetime of a storage device is compared to the amount of primary energy required to build the device, CAES surpasses pumped hydropower energy storage and is vastly superior to electrochemical batteries, which require 10 to 100 times more embodied energy for a given storage capacity. 3
+
+			This is a crucial advantage, because high energy use for the production of the energy storage can greatly decrease the sustainability of a renewable power grid
+			The Problem with Compressed Air
+
+			In spite of these advantages, there are currently only two large-scale CAES plants in operation worldwide: one in Germany, built in 1979, and one in the USA, built in 1991. 4 This limited uptake is mainly attributed to the fact that more than half of the energy is lost when charging and discharging a compressed air “battery”.
+			</p>
+			
+			<h3>Delay</h3>
+
+			<p>
+			While pumped hydropower storage has a charge/discharge efficiency of 70-85%, and chemical batteries reach 65-90%, the CAES plants in operation in Germany and the US have an electric-to-electric efficiency of only 40-42% and 51-54%, respectively. 2 5 6
+
+			The low energy conversion efficiency is mainly due to the fact that air increases in temperature when being compressed to high pressures (both CAES plants operate at 50-70 bar, which is 10 to 20 times the air pressure in a bicycle tyre). Because the energy density of air decreases with rising temperature, both CAES plants remove the heat prior to storage and dump it into the atmosphere. This implies a significant source of energy loss.That’s why many people are seeing a promising alternative in Compressed Air Energy Storage (CAES), another form of mechanical energy storage. In these systems, electricity is used to compress air, which is stored in an underground cavern. To make use of the stored energy, the air is decompressed and converted back to electricity.
+
+			Although CAES also requires favourable geography to provide the underground air storage caverns, it is believed that there are many more suitable sites worldwide than for pumped hydropower Energy storage. 3
+
+			If the energy stored over the lifetime of a storage device is compared to the amount of primary energy required to build the device, CAES is vastly superior to electrochemical batteries
+
+			Importantly, CAES is the most sustainable energy storage around. Unlike pumped hydropower energy storage, compressed air energy storage presents no environmental issues caused by the flooding of land and the damming of rivers.
+
+			Furthermore, if the energy stored over the lifetime of a storage device is compared to the amount of primary energy required to build the device, CAES surpasses pumped hydropower energy storage and is vastly superior to electrochemical batteries, which require 10 to 100 times more embodied energy for a given storage capacity. 3
+
+			This is a crucial advantage, because high energy use for the production of the energy storage can greatly decrease the sustainability of a renewable power grid
+			The Problem with Compressed Air
+
+			In spite of these advantages, there are currently only two large-scale CAES plants in operation worldwide: one in Germany, built in 1979, and one in the USA, built in 1991. 4 This limited uptake is mainly attributed to the fact that more than half of the energy is lost when charging and discharging a compressed air “battery”.
+
+			While pumped hydropower storage has a charge/discharge efficiency of 70-85%, and chemical batteries reach 65-90%, the CAES plants in operation in Germany and the US have an electric-to-electric efficiency of only 40-42% and 51-54%, respectively. 2 5 6
+
+			The low energy conversion efficiency is mainly due to the fact that air increases in temperature when being compressed to high pressures (both CAES plants operate at 50-70 bar, which is 10 to 20 times the air pressure in a bicycle tyre). Because the energy density of air decreases with rising temperature, both CAES plants remove the heat prior to storage and dump it into the atmosphere. This implies a significant source of energy loss.
+			</p>			
+
+		</div>
+	</article>
+
+
+<!-- FICHIERS ASSOCIÉS -->
+<aside class="container" id="fichiers-associes">
+
+
+  <h2>Fichiers associés</h2>
+		
+
+		<ul>
+			<li>
+			<a href="#">
+				<?php include('assets/svg/pdf.svg'); ?>
+				<p>Girard_Shinrai_20181210.pdf</p>
+			</a>
+			</li>
+			<li>
+			<a href="#">
+				<?php include('assets/svg/csv.svg'); ?>
+				<p>shinrai_data.csv</p>
+			</a>
+			</li>
+
+		</ul>
+
+
+
+</aside>
+
+
+<!-- PERSONNES LIÉES -->
+<aside class="container" id="elements-associes">
+
+
+  <h2>Éléments associés</h2>
+  <input type="checkbox" id="checkbox-equipe" checked/>
+	<label for="checkbox-equipe">Équipe</label>
+	<input type="checkbox" id="checkbox-publication" checked/>
+	<label for="checkbox-publication">Publications</label>
+
+	  <ul id="ul-content">
+
+		<li class="equipe">
+		<a href="#">
+			<img src="pessoa.jpg" alt="">
+			<hgroup>
+			<h1>Paul Girard</h1>
+			<p>Ingénieur, Directeur Technique</p>
+			</hgroup>
+			<p class="type">Équipe du Médialab</p>
+		</a>
+		</li>
+
+		<li class="publication">
+		<a href="#">
+			<div class="img-generative">
+			<?php include('assets/images/sample_txt/naturpradi_s.html'); ?>
+			</div>
+			<hgroup>				
+				<p class="titre">Naturpardi</p>			
+				<h1>Le numérique change-t-il les formes de nature urbaine ?</h1>
+			</hgroup>
+			<p class="type">Type publication – 01/01/2018</p>
+		</a>
+		</li>
+
+		<li class="publication">
+		<a href="#">
+			<div class="img-generative">
+			<?php include('assets/images/sample_txt/fabrique_loi_s.html'); ?>
+			</div>
+			<hgroup>				
+				<p class="titre">La fabrique de la loi</p>			
+				<h1>Les parlementaires font-ils la loi ?</h1>
+			</hgroup>
+			<p class="type">Type publication – 01/01/2018</p>
+		</a>
+		</li>
+
+		<li class="equipe">
+		<a href="#">
+			<img src="pessoa.jpg" alt="">
+			<hgroup>
+			<h1>Donato Ricci</h1>
+			<p>Designer Principal</p>
+			</hgroup>
+			<p class="type">Équipe du Médialab</p>
+		</a>
+		</li>
+
+		<li class="equipe">
+		<a href="#">
+			<img src="pessoa.jpg" alt="">
+			<hgroup>
+				<h1>Benjamin Ooghe Tabanou</h1>
+				<p>Ingénieur de recherche</p>
+			</hgroup>
+			<p class="type">Équipe du Médialab</p>
+		</a>
+		</li>
+
+
+
+		<li class="publication">
+		<a href="#">
+			<div class="img-generative">
+			<?php include('assets/images/sample_txt/shinrai_1_s.html'); ?>
+			</div>
+			<hgroup>				
+				<p class="titre">Shinrai</p>			
+				<h1>Quelle est la question de recherche de ce projet en quelques mots ?</h1>
+			</hgroup>
+			<p class="type">Type publication – 01/01/2018</p>
+		</a>
+		</li>
+
+
+
+	</ul>
+
+
+</aside>
+
+
+		
+	
+</body>
+</html>

b4606092c9b92b6166883b128d611cd959839b7b
Yomguithereal <guillaumeplique@gmail.com>
2018-12-20 18:57:51 +0100

    Fixing docker entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ echo "First gatsby build..."
 cd /website/site/ && su-exec node:node /website/site/node_modules/.bin/gatsby build
 cd -
 echo "Starting hydrate:json..."
-[ ! -f data/people.json ] && npm run hydrate:json
+[ ! -f data/people.json ] && npm run hydrate
 echo "Starting server..."
 su-exec node:node /usr/local/bin/npm start

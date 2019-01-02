4e0ac8a8a28d1e6d0b3ba808b946298f9cfe4787
Paul Girard <paul.girard@sciencespo.fr>
2019-01-02 18:10:06 +0100

    Spire translation + id spire for people

--- api/spire.js
+++ api/spire.js
@@ -4,12 +4,21 @@ const config = require('config'),
       fs = require('fs-extra'),
       Ajv = require('ajv'),
       async = require('async'),
-      request = require('request');
+      request = require('request'),
+      _ = require('lodash'),
+      slug = require('slug');
 
 const DATA_PATH = config.get('data');
 
 const models = require('../specs/models.json');
 const spireTypes = require('../specs/spireProductionsTypes.json');
+
+const DEFAULT_MAX_SLUG_TOKENS = 6;
+function slugify(text) {
+  const s = slug(text, {lower: true});
+  return s.split('-').slice(0, DEFAULT_MAX_SLUG_TOKENS).join('-');
+}
+
 
 const VALIDATORS = {};
 
@@ -19,6 +28,54 @@ models.forEach(model => {
 });
 
 const resultPerPage = 2000;
+
+const title = record => record.title + (record.title_sub ? ' - ' + record.title_sub : '');
+
+// translation functions stored by object path.
+// translation function returns false is ther is nothing to update for the path.
+const translators = {
+  'type': record => spireTypes[record.spire_document_type],
+  'date': record => record.date_issued,
+  'title.en': title,
+  'title.fr': title,
+  'content': record => {
+    const content = false;
+    if (record.descriptions) {
+      record.descriptions.map(d => {
+        if (d.language === 'fr' || d.language === 'en')
+          content[d.language] = d.value;
+      });
+    }
+    return content;
+  },
+  'authors': record => record.creators.filter(c => c.role === 'aut').map(c => `${c.agent.name_given} ${c.agent.name_family}`).join(', '),
+  // people:
+  'ref': record => record.citations.html.chicago,
+  'url': record => {
+    if (record.resources && record.resources.length >= 1)
+      return record.resources[0].url;
+    else
+      return config.spire.front + record.rec_id;
+  },
+  'description.fr': record => {
+    return record.keywords && record.keywords.fr ? record.keywords.fr.join(', ') : false;
+  },
+  'description.en': record => {
+    return record.keywords && record.keywords.en ? record.keywords.en.join(', ') : false;
+  }
+};
+
+function translateRecord(record) {
+  const newO = {};
+  for (const field in translators) {
+    const v = translators[field](record);
+    if (v)
+      _.set(newO, field, v);
+  }
+  return newO;
+}
+
+module.exports = translators;
 
 module.exports = function aSPIRE(dataDir = DATA_PATH) {
   // load existing productions indexed by spireId
@@ -45,7 +102,7 @@ module.exports = function aSPIRE(dataDir = DATA_PATH) {
       result_offset: resultOffset
     }]};
     console.debug('request to spire', resultOffset);
-    request.post(config.get('spireAPI'), {body, json: true}, done);
+    request.post(config.spire.api, {body, json: true}, done);
   }, (response) => {
     const r = response.body.result;
     console.debug(`got ${r.result_batch_size}`);
@@ -74,34 +131,20 @@ module.exports = function aSPIRE(dataDir = DATA_PATH) {
       }
       // if new publication + if type is not translated to null
       if (!p && spireTypes[record.spire_document_type]) {
-        // create the object
-        const newProduction = {
-          type: spireTypes[record.spire_document_type],
-          date: record.date_issued,
-          // only one title for publication
-          title: {
-            en: record.title + (record.title_sub ? ' - ' + record.title_sub : ''),
-            fr: record.title + (record.title_sub ? ' - ' + record.title_sub : '')},
-          // slugs:,
-          // lastUpdated:,
-          // authors:,
-          // people:,
-          // ref:,
-          // url:,
-          // draft:,
-          spire: {
-            id: record.rec_id,
-            meta: record
-          }
+        // create the object by translating it to our data model
+        const newProduction = translateRecord(record);
+        // draft by default
+        newProduction.draft = true;
+        // meta
+        newProduction.spire = {
+          id: record.rec_id,
+          meta: record
         };
-        // content
-        if (record.descriptions) {
-          newProduction.content = {};
-          record.descriptions.map(d => {
-            if (d.language === 'fr' || d.language === 'en')
-              newProduction.content[d.language] = d.value;
-          });
-        }
+        // slugs
+        newProduction.slugs = [slugify(newProduction.title ? (newProduction.title.fr || newProduction.title.en || '') : '')];
+        // lastUpdated
+        newProduction.lastUpdated = new Date(record.rec_modified_date).getTime();
+
         newProductions.push(newProduction);
       }
       done();
@@ -111,6 +154,82 @@ module.exports = function aSPIRE(dataDir = DATA_PATH) {
       // validate
       // write everything back
       fs.writeJsonSync(path.join(dataDir, 'newProductions.json'), {productions: productions.concat(newProductions)}, {spaces: 2, encoding: 'utf-8'});
+    });
+  });
+};
+
+module.exports = function aspireAuthors(outDir = '.') {
+  // load existing authors indexed by spireId
+  // const people = {};
+  // fs.readJsonSync(path.join(DATA_PATH, 'people.json'), 'utf-8').people.map(p => {
+  //   people[fingerprint(p.slugs[0])] = p;
+  // });
+  return new Promise((resolve, reject) => {
+    // call SPIRE API
+    let resultOffset = 0;
+    async.doUntil((done) => {
+      // request spire API
+      const body = {jsonrpc: '2.0', method: 'search', id: 1,
+      params: ['corpus', {
+        filter_class: 'Person',
+        result_batch_size: 2000,
+        search_terms: {
+            index: 'affiliation_id',
+            operator: '=',
+            value: '2441/53r60a8s3kup1vc9kf4j86q90'},
+        result_offset: resultOffset
+      }]};
+      console.debug('request to spire', resultOffset);
+      request.post(config.spire.api, {body, json: true}, done);
+    }, (response) => {
+      const r = response.body.result;
+      console.debug(`got ${r.result_batch_size}`);
+      // test if a new page is needed
+      if (r.result_batch_size < resultPerPage) {
+        // we are done
+        return true;
+      }
+      // need more results
+      resultOffset += resultPerPage;
+      return false;
+    }, (err, response) => {
+      const spirePeople = {};
+      if (err) {
+        //work done
+        reject('error in spire api');
+      }
+      response.body.result.records.map(p => {
+        let spireSlug = slugify(`${p.name_given} ${p.name_family}`);
+        switch (spireSlug) {
+          case 'benjamin-ooghe':
+            spireSlug = 'benjamin-ooghe-tabanou';
+            break;
+          case 'vincent-lepinay':
+            spireSlug = 'vincent-antonin-lepinay';
+            break;
+          case 'davy-braun':
+            spireSlug = 'davy-peter-braun';
+            break;
+          default:
+            break;
+        }
+        spirePeople[spireSlug] = p.rec_id;
+      });
+      resolve(spirePeople);
+      //treat records
+      // async.each(spireData.records, (record, done) => {
+      //   const spireSlug = fingerprint(`${record.name_given} ${record.name_family}`);
+      //   const similars = _.keys(people).map(p => {
+      //     return {eudex: eudex.distance(p, spireSlug), overlap: overlap(p, spireSlug), slug: p};
+      //   }).sort((a,b) => {
+      //     if (b.overlap != a.overlap)
+      //       return b.overlap - a.overlap;
+      //     else
+      //       return a.eudex - b.eudex;
+      //   }).filter(c => c.eudex < 100 || c.overlap > 0.88);
+      //   console.debug(spireSlug, similars);
+      //   done();
+      // });
     });
   });
 };

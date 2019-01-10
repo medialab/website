46ac8a4457d07f09a13396ea5abdbf2ce073bec5
Yomguithereal <guillaumeplique@gmail.com>
2019-01-10 17:57:32 +0100

    Fix #129

--- admin/src/sockets.js
+++ admin/src/sockets.js
@@ -1,21 +1,16 @@
 /* global API_URL */
 import io from 'socket.io-client';
 
-let SOCKET = null;
-
 function join(a) {
   return a.replace(/\/$/, '') + '/sockets';
 }
 
 export function acquireSocket() {
-  if (SOCKET)
-    return SOCKET;
-
   const parsed = new URL(API_URL);
 
-  SOCKET = io(parsed.origin, {
+  const socket = io(parsed.origin, {
     path: join(parsed.pathname)
   });
 
-  return SOCKET;
+  return socket;
 }

b9f377637a92684c94db79015f898a2e7300963a
BenjmnG <Benjmng@gmail.com>
2019-01-11 17:52:16 +0100

    Friday!

--- prototyping-clean/fragments/page_liste/_filtre_objet_actualite.php
+++ prototyping-clean/fragments/page_liste/_filtre_objet_actualite.php
@@ -1,24 +1,16 @@
-<div id="filtre_objet_actualite" class="filtre_objet">
-	<ul >
-		<li>
-			<ul>
-				<li>
-					<input type="checkbox" id="filter_publication" name="filter_publication" value="publication">
-	  				<label for="filter_publication">Publication</label>
-				</li>
-				<li>
-					<input type="checkbox" id="filter_web" name="filter_web" value="web">
-	  				<label for="filter_web">Web</label>
-	  			</li>
-				<li>
-					<input type="checkbox" id="filter_outils" name="filter_outils" value="outils">
-	  				<label for="filter_outils">Outils</label>			
-				</li>
-				<li>
-					<input type="checkbox" id="filter_situation" name="filter_situation" value="situation">
-	  				<label for="filter_situation">Situation</label>
-				</li>
-			</ul>
-		</li>
-	</ul>
-</div>
+<!-- Filtre for phone -->
+<input type="checkbox" class="toggle-filtre-phone" id="toggle-filtre-phone" name="toggle-filtre-phone" value="visible"  hidden>
+<label class="toggle-filtre-phone filtre_title" for="toggle-filtre-phone" title="Découvrir les options de filtrage">
+	<p>Filtre<span>⋀</span>	</p>
+</label>
+<!-- End Filtre for phone -->
+
+
+<input type="checkbox" class="filtre_objet" id="filtre_publication" name="filtre_publication" value="publication" >
+<label  class="filtre_objet" for="filtre_publication">Publication</label>
+<input type="checkbox" class="filtre_objet" id="filtre_web" name="filtre_web" value="web" >
+<label  class="filtre_objet" for="filtre_web">Web</label>
+<input type="checkbox" class="filtre_objet" id="filtre_outils" name="filtre_outils" value="outils" >
+<label  class="filtre_objet" for="filtre_outils">Outils</label>
+<input type="checkbox" class="filtre_objet" id="filtre_situation" name="filtre_situation" value="situation" >
+<label  class="filtre_objet" for="filtre_situation">Situation</label>
--- prototyping-clean/fragments/page_liste/_liste_objet.php
+++ prototyping-clean/fragments/page_liste/_liste_objet.php
@@ -2,183 +2,185 @@
 
 	<?php include('_filtre_objet_'.$liste_nature.'.php'); ?>
 
+	<p class="accroche-titre-phone">Description en une phrase de la catégorie <?php echo $liste_nature ?>.</p>
+
 	<ul id="liste_objet">
 
 		<!--<div class="sort">
 			<button id="sort_publication_AZ"><p>▽</p></button>
 			<button id="sort_publication_date"><p>▽</p></button>
 		</div>-->
-		<li data-item="1">
+		<li data-item="1" data-type="publication">
 			<a href="page_objet.php">
 				<h1>Le numérique change-t-il les formes de nature urbaine ?</h1>
 			</a>
 			<ul>
-				<li class="title"><a href="page_objet.php">NATURPRADI</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">NATURPRADI</a></li>
+				<li Class="type">Publication</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php" title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="1">Nature(s) urbaine(s) en pratique(s) digitale(s)- étudie le retour de la nature en ville au prisme de ses effets sociaux, politiques et technologiques notamment grâce aux nouvelles technologies et dispositifs participatifs.</li>
 		
-		<li data-item="2">
+		<li data-item="2"  data-type="outils">
 			<a href="page_objet.php">
 				<h1>Quelle est la question de recherche de ce projet en quelques mots ? </h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">SHINRAI</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">SHINRAI</a></li>
+				<li Class="type">Outils</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link"><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="2">La catastrophe de Fukushima a provoqué une crise politique et sociétale majeure au Japon. SHINRAI analyse la gestion faite par les autorités japonaises et sa réception par les populations.</li>
-		<li data-item="3">
+		<li data-item="3" data-type="publication">
 			<a href="page_objet.php">
 				<h1>Les parlementaires font-ils la loi ?</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">La fabrique de la loi</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">La fabrique de la loi</a></li>
+				<li Class="type">Production</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="3">Ouvrir la boîte noire de la procédure législative en France grâce à l’exploration des données parlementaires. A destination des chercheurs, ce dispositif est également accessible à tous les citoyens et démocratise la compréhension des différentes étapes de fabrique de la loi.</li>
-		<li data-item="4">
+		<li data-item="4" data-type="Web">
 			<a href="page_objet.php">
 				<h1>Titre</h1>
 				<p>Date — Type d'article</p>
 			</a>
 		</li>
 		<li class="item_accroche" data-item-accroche="4">Voici une phrase spécifique sur le projet 4.</li>
-		<li data-item="5">
+		<li data-item="5" data-type="Situation">
 			<a href="page_objet.php">
 				<h1>Titre</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">titre</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">titre</a></li>
+				<li Class="type">Situation</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="5">Voici une phrase spécifique sur le projet 5.</li>
-		<li data-item="6">
+		<li data-item="6"  data-type="publication">
 			<a href="page_objet.php">
 				<h1>Titre</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">titre</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">titre</a></li>
+				<li Class="type">Publication</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="6">Voici une longue phrase spécifique sur le projet 6.</li>
-		<li data-item="7">
+		<li data-item="7" data-type="situation">
 			<a href="page_objet.php">
 				<h1>Titre</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">titre</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">titre</a></li>
+				<li Class="type">Situation</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="7">Voici une phrase spécifique sur le projet 7.</li>
-		<li data-item="8">
+		<li data-item="8"  data-type="Web">
 			<a href="page_objet.php">
 				<h1>Titre</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">titre</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">titre</a></li>
+				<li Class="type">Web</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="8">Voici une phrase spécifique sur le projet 8.</li>
-		<li data-item="9">
+		<li data-item="9"  data-type="web">
 			<a href="page_objet.php">
 				<h1>Titre</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">titre</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">titre</a></li>
+				<li Class="type">Web</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="9">Voici une phrase spécifique sur le projet 9.</li>
-		<li data-item="10">
+		<li data-item="10"  data-type="outils">
 			<a href="page_objet.php">
 				<h1>Titre</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">titre</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">titre</a></li>
+				<li Class="type">Outils</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="10">Voici une longue phrase spécifique sur le projet 10.</li>
-		<li data-item="11">
-			<a href="page_objet.php">
+		<li data-item="11"  data-type="outils">
+			<a href="page_objet.php" >
 				<h1>Titre</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">titre</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">titre</a></li>
+				<li Class="type">Outils</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="11">Voici une phrase spécifique sur le projet 11.</li>
-		<li data-item="12">
+		<li data-item="12"  data-type="outils">
 			<a href="page_objet.php">
 				<h1>Titre</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">titre</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">titre</a></li>
+				<li Class="type">Outils</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="12">Voici une phrase spécifique sur le projet 12.</li>
-		<li data-item="13">
+		<li data-item="13"  data-type="outils">
 			<a href="page_objet.php">
 				<h1>Titre</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">titre</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">titre</a></li>
+				<li Class="type">Outils</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="13">Voici une phrase spécifique sur le projet 13.</li>
-		<li data-item="14">
+		<li data-item="14" data-type="publication">
 			<a href="page_objet.php">
 				<h1>Titre</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">titre</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">titre</a></li>
+				<li Class="type">Publication</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
 		</li>
 		<li class="item_accroche" data-item-accroche="14">Voici une longue phrase spécifique sur le projet 14.</li>
-		<li data-item="15">
+		<li data-item="15" data-type="publication">
 			<a href="page_objet.php">
 				<h1>Titre</h1>
 			</a>
 			<ul>
-				<li class="title" ><a href="page_objet.php">titre</a></li>
-				<li Class="type">type publication</li>
+				<li class="activite" title="aller sur la page activité dédiée" ><a href="page_objet.php">titre</a></li>
+				<li Class="type">Publication</li>
 				<li class="time" data-time="2018-01-01"> 01/01/2018</li>
 				<li class="direct_link" ><a href="page_objet.php"title="aller sur le site dédié" >↗</a></li>
 			</ul>
@@ -186,19 +188,7 @@
 		<li class="item_accroche" data-item-accroche="15">Voici une phrase spécifique sur le projet 15.</li>
 
 		<!-- Keep accroche here -->
-		<li class="item_accroche accroche-titre">Description en une phrase de la catégorie publication.</li>
-		
-		<li class="item_accroche" data-item-accroche="4">Voici une phrase spécifique sur le projet 4.</li>
-		<li class="item_accroche" data-item-accroche="5">Voici une phrase spécifique sur le projet 5.</li>
-		<li class="item_accroche" data-item-accroche="6">Voici une longue phrase spécifique sur le projet 6.</li>
-		<li class="item_accroche" data-item-accroche="7">Voici une phrase spécifique sur le projet 7.</li>
-		<li class="item_accroche" data-item-accroche="8">Voici une phrase spécifique sur le projet 8.</li>
-		<li class="item_accroche" data-item-accroche="9">Voici une phrase spécifique sur le projet 9.</li>
-		<li class="item_accroche" data-item-accroche="10">Voici une longue phrase spécifique sur le projet 10.</li>
-		<li class="item_accroche" data-item-accroche="11">Voici une phrase spécifique sur le projet 11.</li>	
-		<li class="item_accroche" data-item-accroche="12">Voici une phrase spécifique sur le projet 12.</li>
-		<li class="item_accroche" data-item-accroche="13">Voici une phrase spécifique sur le projet 13.</li>
-		<li class="item_accroche" data-item-accroche="14">Voici une longue phrase spécifique sur le projet 14.</li>
-		<li class="item_accroche" data-item-accroche="15">Voici une phrase spécifique sur le projet 15.</li>
+		<li class="item_accroche accroche-titre">Description en une phrase de la catégorie <?php echo $liste_nature ?>.</li>
+
 	</ul>
 </section>

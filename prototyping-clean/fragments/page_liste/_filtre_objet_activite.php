d93ff712bef107938210c7b392b04f08cc90eff3
JulieBlanc <contact@julie-blanc.fr>
2019-01-16 01:51:24 +0100

    pages listing avec filtres

--- prototyping-clean/fragments/page_liste/_filtre_objet_activite.php
+++ prototyping-clean/fragments/page_liste/_filtre_objet_activite.php
@@ -1,3 +1,8 @@
+
+<h1 class="type_title" id="activite_title">Activités</h1>
+
+
+
 <!-- Filtre for phone -->
 <input type="checkbox" class="toggle-filtre-phone" id="toggle-filtre-phone" name="toggle-filtre-phone" value="visible"  hidden>
 <label class="toggle-filtre-phone filtre_title" for="toggle-filtre-phone" title="Découvrir les options de filtrage">
@@ -6,12 +11,51 @@
 <!-- End Filtre for phone -->
 
 
-<input type="checkbox" class="filtre_objet" id="filtre_recherche" name="filtre_publication" value="recherche" >
-<label  class="filtre_objet" for="filtre_recherche">Recherche</label>
+<!-- Trois possibilité dans le build
+-----------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
+-->
 
-<input type="checkbox" class="filtre_objet" id="filtre_web" name="filtre_web" value="web" >
-<label  class="filtre_objet" for="filtre_web">Web</label>
-<input type="checkbox" class="filtre_objet" id="filtre_outils" name="filtre_outils" value="outils" >
-<label  class="filtre_objet" for="filtre_outils">Outils</label>
-<input type="checkbox" class="filtre_objet" id="filtre_situation" name="filtre_situation" value="situation" >
-<label  class="filtre_objet" for="filtre_situation">Situation</label>
+<!-- Active (par défaut) -->
+<input type="checkbox" id="filtre_activite_passe" name="filtre_activite_active" value="activite_active" hidden checked>
+<label class="filtre_objet filtre_activite" for="filtre_activite_active"><a class="checkbox-medialab checkbox-medialab_link" href="linkPageActivitesActives">Actives</a></label>
+
+<input type="checkbox" id="filtre_activite_passe" name="filtre_activite_passe" value="activite_passe" hidden >
+<label class="filtre_objet filtre_activite" for="filtre_activite_passe"><a class="checkbox-medialab checkbox-medialab_link" href="linkPageActivitesActives+Passees">Passées</a></label>
+
+
+<!-- Active + Passees
+<input type="checkbox" id="filtre_activite_passe" name="filtre_activite_active" value="activite_active" hidden checked>
+<label class="filtre_objet filtre_activite" for="filtre_activite_active"><a class="checkbox-medialab checkbox-medialab_link" href="linkPageActivitesPassees">Actives</a></label>
+
+<input type="checkbox" id="filtre_activite_passe" name="filtre_activite_passe" value="activite_passe" hidden checked>
+<label class="filtre_objet filtre_activite" for="filtre_activite_passe"><a class="checkbox-medialab checkbox-medialab_link" href="linkPageActivitesActives">Passées</a></label>
+ -->
+
+<!-- Passees 
+<input type="checkbox" id="filtre_activite_passe" name="filtre_activite_active" value="activite_active" hidden>
+<label class="filtre_objet filtre_activite" for="filtre_activite_active"><a class="checkbox-medialab checkbox-medialab_link" href="linkPageActivitesActives+Passees">Actives</a></label>
+
+<input type="checkbox" id="filtre_activite_passe" name="filtre_activite_passe" value="activite_passe" hidden checked>
+<label class="filtre_objet filtre_activite" for="filtre_activite_passe"><a class="checkbox-medialab checkbox-medialab_link" href="linkPageActivitesActives">Passées</a></label>
+-->
+
+
+<!-- Filtrer par type
+-----------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
+-->
+
+<h2 class="filtre_objet filtre_activite_title">Filtrer par type</h2>
+
+<input type="checkbox" id="filtre_recherche" name="filtre_recherche" value="recherche" hidden >
+<label class="filtre_objet filtre_activite checkbox-medialab" for="filtre_recherche">Recherche</label>
+
+
+<input type="checkbox" id="filtre_enseignement" name="filtre_enseignement" value="enseignement" hidden >
+<label class="filtre_objet filtre_activite checkbox-medialab" for="filtre_enseignement">Enseignement</label>
+
+<input type="checkbox" id="filtre_methode" name="filtre_methode" value="methode" hidden >
+<label class="filtre_objet filtre_activite checkbox-medialab" for="filtre_methode">Méthode</label>
--- prototyping-clean/fragments/page_liste/_filtre_objet_production.php
+++ prototyping-clean/fragments/page_liste/_filtre_objet_production.php
@@ -1,3 +1,8 @@
+
+<h1 class="type_title">Productions</h1>
+
+
+
 <!-- Filtre for phone -->
 <input type="checkbox" class="toggle-filtre-phone" id="toggle-filtre-phone" name="toggle-filtre-phone" value="visible"  hidden>
 <label class="toggle-filtre-phone filtre_title" for="toggle-filtre-phone" title="Découvrir les options de filtrage">
@@ -6,11 +11,84 @@
 <!-- End Filtre for phone -->
 
 
-<input type="checkbox" class="filtre_objet" id="filtre_publication" name="filtre_publication" value="publication" >
-<label  class="filtre_objet" for="filtre_publication">Publication</label>
-<input type="checkbox" class="filtre_objet" id="filtre_web" name="filtre_web" value="web" >
-<label  class="filtre_objet" for="filtre_web">Web</label>
-<input type="checkbox" class="filtre_objet" id="filtre_outils" name="filtre_outils" value="outils" >
-<label  class="filtre_objet" for="filtre_outils">Outils</label>
-<input type="checkbox" class="filtre_objet" id="filtre_situation" name="filtre_situation" value="situation" >
-<label  class="filtre_objet" for="filtre_situation">Situation</label>
+<div id="go-to-year">
+	<h1>Aller à l'année</h1>
+	<input type="checkbox" id="go-to-year-input" name="go-to-year-input" value="go-to-year-input" hidden>
+	<label for="go-to-year-input"><span></span></label>
+	<ul>
+		<li><a href="#year-2018">2018</a></li>
+		<li><a href="#year-2017">2017</a></li>
+		<li><a href="#year-2016">2016</a></li>
+		<li><a href="#year-2015">2015</a></li>
+		<li><a href="#year-2014">2014</a></li>
+		<li><a href="#year-2013">2013</a></li>
+	</ul>
+</div>
+
+<!-- Productions -->
+<input type="checkbox" id="filtre_publication" name="filtre_publication" value="publication" hidden checked>
+<label class="filtre_objet filtre_production" for="filtre_publication"><a class="checkbox-medialab checkbox-medialab_link" href="linkPageProductionsPublications">Publications <span>〉</span></a></label>
+
+
+<input type="checkbox" id="filtre_publication_article" name="filtre_publication_article" value="publication_article" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_publication_article">Articles</label>
+
+<input type="checkbox" id="filtre_publication_communication" name="filtre_publication_communication" value="publication_communication" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_publication_communication">Communications</label>
+
+<input type="checkbox" id="filtre_publication_book" name="filtre_publication_book" value="publication_book" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_publication_book">Books</label>
+
+<input type="checkbox" id="filtre_publication_thesis" name="filtre_publication_thesis" value="publication_thesis" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_publication_thesis">Thesis</label>
+
+<input type="checkbox" id="filtre_publication_grey" name="filtre_publication_grey" value="publication_grey" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_publication_grey">Grey</label> 
+
+
+
+<!-- Éditions web -->
+<input type="checkbox" id="filtre_editionweb" name="filtre_editionweb" value="editionweb" hidden>
+<label class="filtre_objet filtre_production" for="filtre_editionweb"><a class="checkbox-medialab checkbox-medialab_link" href="linkPageProductionsEditionsweb">Éditions web <span>〉</span></a></label>
+
+<!--
+<input type="checkbox" id="filtre_editionweb_datascape" name="filtre_editionweb_datascape" value="editionweb_datascape" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_editionweb_datascape">Datascape</label>
+
+<input type="checkbox" id="filtre_editionweb_website" name="filtre_editionweb_website" value="editionweb_website" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_editionweb_website">Website</label>
+-->
+
+<!-- Tools-->
+<input type="checkbox" id="filtre_tools" name="filtre_tools" value="tools" hidden>
+<label class="filtre_objet filtre_production" for="filtre_tools"><a class="checkbox-medialab checkbox-medialab_link" href="linkPageTools">Tools <span>〉</span></a></label>
+
+<!--
+<input type="checkbox" id="filtre_tools_software" name="filtre_tools_software" value="tools_software" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_tools_software">Software</label>
+
+<input type="checkbox" id="filtre_tools_code" name="filtre_tools_code" value="tools_code" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_tools_code">Code</label>
+-->
+
+<!-- Situations -->
+<input type="checkbox" id="filtre_situations" name="filtre_situations" value="situations" hidden>
+<label class="filtre_objet filtre_production" for="filtre_situations"><a class="checkbox-medialab checkbox-medialab_link" href="linkPageSituations">Situations <span>〉</span></a></label>
+
+<!--
+<input type="checkbox" id="filtre_situations_exhibition" name="filtre_situations_exhibition" value="situations_exhibition" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_situations_exhibition">Exhibition</label>
+
+<input type="checkbox" id="filtre_situations_workshop" name="filtre_situations_workshop" value="situations_workshop" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_situations_workshop">Workshop</label>
+
+<input type="checkbox" id="filtre_situations_simulation" name="filtre_situations_simulation" value="situations_simulation" hidden >
+<label class="filtre_objet_value filtre_production checkbox-medialab" for="filtre_situations_simulation">Simulation</label>
+
+<input type="checkbox" id="filtre_situations_conference" name="filtre_situations_conference" value="situations_conference" hidden >
+<label class="filtre_objet_value filtre_situations checkbox-medialab" for="filtre_situations_conference">Conférence</label>
+-->
+
+<!-- Médias -->
+<input type="checkbox" id="filtre_media" name="filtre_media" value="media" hidden>
+<label class="filtre_objet filtre_production" for="filtre_media"><a class="checkbox-medialab checkbox-medialab_link" href="linkPageMedia">Média <span>〉</span></a></label>

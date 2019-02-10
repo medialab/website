5ebccce2012f869155e3b921784606104b6da3a8
Thomas Parisot <oncletom@users.noreply.github.com>
2019-02-10 20:25:08 +0100

    Fix : affiche les descriptions des productions sous forme de HTML

--- site/src/components/ProductionListing.js
+++ site/src/components/ProductionListing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'gatsby';
 
 import FilterProduction from './fragments/pageListe/FilterProduction.js';
+import RawHTML from './RawHtml.js';
 
 /* import {templateMembership} from './helpers.js';  */
 import './scss/page_liste.scss';
@@ -30,7 +31,7 @@ export default function ProductionListing({lang, list}) {
   			      <p className="date">{p.lastUpdated}</p>
   						<p className="type">{p.type}</p>
   					</li>
-  					<li className="item_accroche description" data-item-accroche="0"><Link to={p.slugs[p.slugs.length - 1]}>{lang === "fr" ? p.description.fr : p.description.en}</Link></li>
+  					<li className="item_accroche description" data-item-accroche="0"><Link to={p.slugs[p.slugs.length - 1]}><RawHTML html={lang === "fr" ? p.description.fr : p.description.en} /></Link></li>
             </>
 				 ))}
 		     </ul>

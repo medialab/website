f560cb1b45f37e5f572815d74710dfbd71ead7ff
BenjmnG <Benjmng@gmail.com>
2019-02-11 12:25:38 +0100

    Filtre Equipe fonctionnelle

--- site/src/components/PeopleListing.js
+++ site/src/components/PeopleListing.js
@@ -16,9 +16,8 @@ export default function PeopleListing({lang, list}) {
 	return (
 		<>
 		<FiltreEquipe />
-		<section id="liste">
-		    <ul className="liste_equipe" id="liste_equipe">
-
+		<section id="liste_equipe">
+		    <ul className="liste_equipe">
 			{list.map(p => (
 	          	<li key={p.id} data-item={p.id} data-domain={p.domain} data-active={p.active ? "yes" : "no"} data-member={p.membership === "member" ? "yes" : "no"}>
 				    <Link to={`/people/${p.slugs[p.slugs.length - 1]}`}>
@@ -36,19 +35,3 @@ export default function PeopleListing({lang, list}) {
 		</>
 	);
 }
-
-/*		
-
-${p.slugs[p.slugs.length - 1]}
-
-
-<li data-item="" data-domaine="recherche" data-statut="actif" data-member="yes">
-    <Link to="/people/dominique-cardon">
-        <figure><img src="/assets/images/sample/D-Cardon-bis.jpg" /></figure>
-        <hgroup>
-		    <h1 data-level-1="name">Dominique Cardon</h1>
-            <h2 data-level-2="role" data-type="role">Directeur du médialab</h2>
-        </hgroup>
-        <p data-type="activite">En ce moment, je chef et j'écris des papiers</p>
-    </Link>
-</li>*/

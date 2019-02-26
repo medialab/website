48b21921289df039ef1b7d21baeb758374494776
Thomas Parisot <oncletom@users.noreply.github.com>
2019-02-26 18:02:27 +0100

    Éléments de navigation pour une Production

--- site/src/components/fragments/Nav.js
+++ site/src/components/fragments/Nav.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import {Link} from 'gatsby';
-/*
-
-import {graphql} from 'gatsby';
-import RawHtml from './RawHtml';
- */
+import {Link, withPrefix} from 'gatsby';
 
 import Logo from '../assets/svg/logo_medialab.svg';
 
+const getRelatedElements = (relatedElements, object) => {
+	return relatedElements.filter(({exist}) => {
+		return !(typeof exist === 'function') || exist(object);
+	});
+}
 
-function Nav({lang, object}) {
 
-
+export default function Nav({lang, object={}, related=[]}) {
 	//Je pense que nous n'avons pas assez de donnée dans le CMS pour mener à bien cette fonction
 	// Néanmoins la logique serait :  Si et seulement si il existe une Image Générée créer cet élément
 	// Cet élément est composé d'une image lambda et de son corrolaire Image Générée
-	let img = null;
+	let coverImage = null;
 	if (object && object.cover) {
-		img = (
+		coverImage = (
 			<div>
-				<img src={object.cover.url} alt={object.cover.title} />
+				<img src={withPrefix(object.cover.file)} alt={object.cover.title} />
 				<div class=".image-generator"></div>
 			</div>
 		);
@@ -33,15 +32,15 @@ function Nav({lang, object}) {
 				</Link>
 			</div>
 			<div className="nav-inside-item" id="img-article">
-				{img}
+				{coverImage}
 			</div>
-			{(object.related || []).map(o => (
-				<div className="nav-inside-item" >
-					<Link to={`#${o.id}`}><p>{lang === "fr" ? o.fr : o.en }</p></Link>
+			{(getRelatedElements(related, object)).map(related => (
+				<div className="nav-inside-item">
+					<p>
+						<a href={`#${related.id}`}>{related[ lang ]}</a>
+					</p>
 				</div>)
 			)}
-
 		</nav>
 	);
 }
-export default Nav;

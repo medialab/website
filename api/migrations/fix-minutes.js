5178ebaf437b775e6372a95fff1bc1627d36d60f
Yomguithereal <guillaumeplique@gmail.com>
2019-02-27 16:24:18 +0100

    Adding migration to fix minutes

---
+++ api/migrations/fix-minutes.js
@@ -0,0 +1,29 @@
+function fixMinutes(date) {
+  if (!date.includes('T'))
+    return date;
+
+  const [d, t] = date.split('T');
+
+  if (!t.includes(':'))
+    return d + 'T' + t + ':00';
+
+  return date;
+}
+
+module.exports = function(req, dbs, next) {
+  dbs.news.read();
+
+  const state = dbs.news.getState();
+
+  state.news.forEach(n => {
+    if (n.startDate)
+      n.startDate = fixMinutes(n.startDate);
+
+    if (n.endDate)
+      n.endDate = fixMinutes(n.endDate);
+  });
+
+  dbs.news.setState(state);
+
+  dbs.news.write().then(() => next());
+};

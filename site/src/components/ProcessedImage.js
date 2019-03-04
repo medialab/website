4e248142a3ce86e2389c987eb2a27dc1547367e8
BenjmnG <Benjmng@gmail.com>
2019-03-04 18:27:46 +0100

    Update ProcessedImage.js

--- site/src/components/ProcessedImage.js
+++ site/src/components/ProcessedImage.js
@@ -4,9 +4,9 @@ const ROWS = {
   small: 25,
   medium: 75,
   large: 150
-};
+}; 
 
-const PLACEHOLDER_CHARACTER = '▒';
+const PLACEHOLDER_CHARACTER = '▒▓';
 
 export default function ProcessedImage({image, size}) {
 
@@ -15,16 +15,16 @@ export default function ProcessedImage({image, size}) {
   const rows = ROWS[size];
 
   return (
-    <code>
+    <>
       {Array.from(new Array(rows), (_, i) => {
         const row = needPlaceholder ?
-          PLACEHOLDER_CHARACTER.repeat(rows) :
+          PLACEHOLDER_CHARACTER.repeat(rows/2) : // Je divise par deux car j'ai ajouté un caractère
           image.slice(i, i + rows);
 
         return (
           <pre key={i}>{row}</pre>
         );
       })}
-    </code>
+    </>
   );
 }

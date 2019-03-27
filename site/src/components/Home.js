11b50e962344438f919b3672a9f82a244e738fc8
BenjmnG <Benjmng@gmail.com>
2019-03-27 19:51:31 +0100

    Unlock Flux component

--- site/src/components/Home.js
+++ site/src/components/Home.js
@@ -6,7 +6,7 @@ import {Link} from 'gatsby';
 import Slideshow from './fragments/index/Slideshow.js';
 import Now from './fragments/index/Now.js';
 import Agenda from './fragments/index/Agenda.js';
-//import Flux from './fragments/index/Flux.js';
+import Flux from './fragments/index/Flux.js';
 
 /*import RawHtml from './RawHtml';*/
 
@@ -22,17 +22,17 @@ export default function Home({lang, grid, slider, rdv}) {
       <main>
         <section id="introduction">
           <h1>Le médialab</h1>
-          <p>
-				Laboratoire de recherche interdisciplinaire, le médialab est un lieu de conception, de développement et d'expérimentation de méthodes numériques hybrides pour nourrir des questions scientifiques ancrées dans le périmètre des Sciences humaines et sociales.
+          <div className="container">
+				    <p>Laboratoire de recherche interdisciplinaire, le médialab est un lieu de conception, de développement et d'expérimentation de méthodes numériques hybrides pour nourrir des questions scientifiques ancrées dans le périmètre des Sciences humaines et sociales.</p>
             <a href="#">En savoir plus</a>
-          </p>
+          </div>
         </section>
         <Now now={grid} lang={lang} />
         <Agenda rdv={rdv} lang={lang} />
         <section id="follow">
           <p>Vous pouvez suivre les activité du Medialab sur <a href="https://twitter.com/medialab_scpo" title="twitter">⟐</a> ou sur <a href="https://github.com/medialab" title="Github">⊶</a></p>
         </section>
-        {/*<Flux />*/}
+        <Flux />
       </main>
     </>
   );

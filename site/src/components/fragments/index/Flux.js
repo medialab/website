0294b5787584f9efc727dc1a7600056a11da90fb
BenjmnG <Benjmng@gmail.com>
2019-03-27 19:52:46 +0100

    Update Flux.js

--- site/src/components/fragments/index/Flux.js
+++ site/src/components/fragments/index/Flux.js
@@ -11,11 +11,11 @@ const Flux = () => {
 
 			<h1>À chaud</h1>
 			<div id="flux-container">
-				<!-- if type=git (bon modèle pour les liens) -->
+				{/*if type=git (bon modèle pour les liens) */}
 				<article data-type="git">
 					<p className="label">Git</p>
 					<h2 data-level-1="title">Tic et Toc</h2>
-					<h3 data-level-2="description">Outils composé de 0 ou de 1, de $ ou de <<, enfin plein de machin pas si complexe...</h3>
+					<h3 data-level-2="description">Outils composé de 0 ou de 1, de $ ou de , enfin plein de machin pas si complexe...</h3>
 					
 					<ul className="details">
 						<li className="language">Javascript / HTML / CSS </li>
@@ -25,10 +25,10 @@ const Flux = () => {
 					</ul>
 				</article>
 
-				<!-- if type=tweet (bon modèle pour les liens) -->
+				{/*if type=tweet (bon modèle pour les liens) */}
 				<article data-type="tweet" >
 					<p className="label">Tweet</p>
-					<!-- <h2 data-level-1="title">Tic et Toc</h2>-->
+					{/*<h2 data-level-1="title">Tic et Toc</h2>*/}
 					<p data-level-2="description">
 						Faire vivre la #démocratie exige que nous ayons #confiance envers les #medias. Comment y parvenir dans un univers où <span className="hashtag">#trolls</span>, <span className="hashtag">#FakeNews</span> et concurrence au nb de <span className="hashtag">clics</span> polluent le paysage ? Réponses le 16 avril lors du 1er rdv de <span className="mention">@ScPoResearch</span> ✍ <Link to="/">https://bit.ly/2UQmrZI</Link>
 					</p>			

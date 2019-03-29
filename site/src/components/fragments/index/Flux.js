5e1eeef9368d579d3e234f9513e953c260457d82
BenjmnG <Benjmng@gmail.com>
2019-03-29 12:35:41 +0100

    Playing with Flux

--- site/src/components/fragments/index/Flux.js
+++ site/src/components/fragments/index/Flux.js
@@ -13,8 +13,9 @@ const Flux = () => {
 			<div id="flux-container">
 				{/*if type=git (bon modèle pour les liens) */}
 				<article data-type="git">
-					<p className="label">Git</p>
-					<h2 data-level-1="title">Tic et Toc</h2>
+					<p className="type">Git</p>
+					<p >&nbsp;</p>
+					<h2 data-level-1="title" data-icon="git">Tic et Toc</h2>
 					<h3 data-level-2="description">Outils composé de 0 ou de 1, de $ ou de , enfin plein de machin pas si complexe...</h3>
 					
 					<ul className="details">
@@ -26,12 +27,12 @@ const Flux = () => {
 				</article>
 
 				{/*if type=tweet (bon modèle pour les liens) */}
-				<article data-type="tweet" >
-					<p className="label">Tweet</p>
+				<article data-type="tweet" className="major">
+					<p className="type">Tweet</p>
 					{/*<h2 data-level-1="title">Tic et Toc</h2>*/}
-					<p data-level-2="description">
+					<h2 data-level-2="description">
 						Faire vivre la #démocratie exige que nous ayons #confiance envers les #medias. Comment y parvenir dans un univers où <span className="hashtag">#trolls</span>, <span className="hashtag">#FakeNews</span> et concurrence au nb de <span className="hashtag">clics</span> polluent le paysage ? Réponses le 16 avril lors du 1er rdv de <span className="mention">@ScPoResearch</span> ✍ <Link to="/">https://bit.ly/2UQmrZI</Link>
-					</p>			
+					</h2>			
 					<ul className="details">
 						<li className="account">@medialab_ScPo</li>
 						<li className="update">Today</li>
@@ -39,18 +40,21 @@ const Flux = () => {
 				</article>
 
 				<article data-type="post" >
-					<p className="label">Tweet</p>
+					<p className="type">site</p>
 					<h2 data-level-1="title">+ 3<span>posts</span></h2>					
 					<ul className="details">
 						<li className="update">Since a week</li>
 					</ul>
-                	<div className="image-background"><ProcessedImage size="medium" image="" /></div>
+                	<div className="image-background"><ProcessedImage size="small" image="" /></div>
 
 				</article>
 				
 				<article></article>							
 				<article></article>							
 				<article></article>							
+				<article></article>							
+				<article></article>							
+				<article></article>				
 				<article></article>							
 				<article></article>							
 				<article></article>							

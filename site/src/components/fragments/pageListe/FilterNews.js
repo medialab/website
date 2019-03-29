f8507a9b7c8546fb3ba2296a11cf897375cdfa28
BenjmnG <Benjmng@gmail.com>
2019-03-29 17:44:45 +0100

    Filtre Actualité
    #230

--- site/src/components/fragments/pageListe/FilterNews.js
+++ site/src/components/fragments/pageListe/FilterNews.js
@@ -1,22 +1,30 @@
 import React from 'react';
-/*import {graphql} from 'gatsby';*/
+/*import {graphql} from 'gatsby';*/ 
 //import {Link} from 'gatsby';
 
 const MAX_NB_FILTER_ITEMS = 12;
 
 const FilterNews = ({lang, years}) => {
 
-  let title, year, filters;
+  let title, year, filters, event, post, notice, typeNews;
   years = years.sort().reverse();
   if (lang === 'fr') {
     title = 'Actualités';
     year = 'Aller à l\'année…';
     filters = 'Filtres';
+    typeNews = "Type d'actualités";
+    event = 'Évenement';
+    post = 'Publication';
+    notice = 'Annonce';
   }
   else {
     title = 'News';
     year = 'Go to year…';
     filters = 'Filters';
+    typeNews = "News‘ type";    
+    event = 'Event';
+    post = 'Post';
+    notice = 'Notice';
   }
   let suppYears = false;
 
@@ -37,6 +45,12 @@ const FilterNews = ({lang, years}) => {
       </label>
 
       <div id="background-phone" />
+      
+      {/* YEAR */}
+      <input
+        type="radio" id="radio_filtre-actu_year" name="radio_filtre-actu"
+        value="year" hidden />
+      <label htmlFor="radio_filtre-actu_year"><span>〉</span></label>
 
       <div className="go-to-year" id="go-to-year_news">
         <p>{ year }</p>
@@ -49,7 +63,20 @@ const FilterNews = ({lang, years}) => {
             <li key={`filter-years-before-${suppYears}`}><a href={`#year-${suppYears}`} >&le; {suppYears}</a></li>
           }
         </ul>
-      </div>
+      </div>   
+      
+      {/* TYPE */}
+      <input type="radio" id="radio_filtre-actu_type" name="radio_filtre-actu" className="input_filtre-actu" value="type" defaultChecked hidden />
+      <label htmlFor="radio_filtre-actu_type">{typeNews} <span>〉</span></label>
+
+      <input type="checkbox" id="filtre-actu_event" name="filtre-actu_event" className="input_filtre-actu" value="event" hidden />
+      <label className="filtre-actu checkbox-medialab"  htmlFor="filtre-actu_event">{event}</label>
+
+      <input type="checkbox" id="filtre-actu_post" name="filtre-actu_post" className="input_filtre-actu" value="post" hidden />
+      <label className="filtre-actu checkbox-medialab"  htmlFor="filtre-actu_post">{post}</label>
+
+      <input type="checkbox" id="filtre-actu_notice" name="filtre-actu_notice" className="input_filtre-actu" value="notice" hidden />
+      <label className="filtre-actu checkbox-medialab"  htmlFor="filtre-actu_notice">{notice}</label>
 
     </>
   );

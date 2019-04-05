7995a0509e2d68a7564e5bb7b308393a6d889957
Yomguithereal <guillaumeplique@gmail.com>
2019-04-05 16:01:22 +0200

    Feeding data to archives Related to #257

--- site/src/components/Archive.js
+++ site/src/components/Archive.js
@@ -6,8 +6,11 @@ import Nav from './fragments/Nav.js';
 import RawHtml from './RawHtml';
 //import './scss/apropos.scss';
 
-export default function Archive({lang}) {
+export default function Archive({lang, activities, news, productions}) {
   console.log(lang);
+  console.log('ACTIVITIES', activities);
+  console.log('NEWS', news);
+  console.log('PRODUCTIONS', productions);
 
   return (
     <>

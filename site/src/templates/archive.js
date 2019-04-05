40a5b7a5fe75c5c453f23f2e2e77bb859c24e358
Yomguithereal <guillaumeplique@gmail.com>
2019-04-05 16:01:22 +0200

    Feeding data to archives Related to #257

--- site/src/templates/archive.js
+++ site/src/templates/archive.js
@@ -4,16 +4,71 @@ import {graphql} from 'gatsby';
 import Layout from '../components/Layout';
 import Archive from '../components/Archive';
 
+export const query = graphql`
+  {
+    allActivitiesJson {
+      edges {
+        node {
+          name
+          permalink {
+            en
+            fr
+          }
+        }
+      }
+    }
+
+    allNewsJson {
+      edges {
+        node {
+          title {
+            en
+            fr
+          }
+          permalink {
+            en
+            fr
+          }
+        }
+      }
+    }
+
+    allProductionsJson {
+      edges {
+        node {
+          title {
+            en
+            fr
+          }
+          permalink {
+            en
+            fr
+          }
+        }
+      }
+    }
+  }
+`;
+
+const getNode = ({node}) => node;
 
 export default ({data, pageContext}) => {
   console.log(data, pageContext);
+
+  const activities = data.allActivitiesJson.edges.map(getNode),
+        news = data.allNewsJson.edges.map(getNode),
+        productions = data.allProductionsJson.edges.map(getNode);
 
   return (
     <Layout
       lang={pageContext.lang}
       className="page-archive"
       permalinks={pageContext.permalinks}>
-      <Archive lang={pageContext.lang} />
+      <Archive
+        lang={pageContext.lang}
+        activities={activities}
+        news={news}
+        productions={productions} />
     </Layout>
   );
 };

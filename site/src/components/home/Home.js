7a1423084bd25ec315aa7d5b3508045b9febe266
BenjmnG <Benjmng@gmail.com>
2019-04-11 12:57:48 +0200

    Flux

--- site/src/components/home/Home.js
+++ site/src/components/home/Home.js
@@ -29,7 +29,7 @@ export default function Home({lang, grid, slider, rdv}) {
         <section id="follow">
           <p>Vous pouvez suivre les activité du Medialab sur <a href="https://twitter.com/medialab_scpo" title="twitter">⟐</a> ou sur <a href="https://github.com/medialab" title="Github">⊶</a></p>
         </section>
-        {/*<Flux />*/}
+        <Flux />
       </main>
     </>
   );
--- site/src/components/home/fragments/Flux.js
+++ site/src/components/home/fragments/Flux.js
@@ -1,21 +1,94 @@
 import React from 'react';
 import {Link} from 'gatsby';
+import ProcessedImage from '../../helpers/ProcessedImage.js';
 
-const Flux = () => {
+export default function flux() {
 	return (
 		<>
-		/*
+		
 		<section id="flux">
 
 			<h1>À chaud</h1>
-			<div id="flux-container">
-	
+
+			<div id="flux-container" style={{ '--h-0': 2, '--h-1': 2, '--h-2': 1, '--h-3': 1}}>
+				{/*if type=git (bon modèle pour les liens) */}
+				<article data-type="git" >
+					<p className="type">Git</p>
+					<p className="update">Today</p>
+
+					<h2 data-level-1="title">Tic et Toc</h2>
+					<h3 data-level-2="description">Outils composé de 0 ou de 1, de $ ou de , enfin plein de machin pas si complexe...</h3>
+					
+					<ul className="details">
+						<li className="language">JS / HTML / CSS </li>
+						<li className="contrib">3 contributeurs</li>
+						<li className="licence">LGPL-3.0</li>
+					</ul>
+				</article>
+
+				{/*if type=tweet (bon modèle pour les liens) */}
+				<article data-type="tweet" className="major" data-lenght="280">
+					<p className="type">Tweet</p>
+					<p className="update">Today</p>
+					{/*<h2 data-level-1="title">Tic et Toc</h2>*/}
+					<h2 data-level-2="description">
+						Faire vivre la #démocratie exige que nous ayons #confiance envers les #medias. Comment y parvenir dans un univers où <span className="hashtag">#trolls</span>, <span className="hashtag">#FakeNews</span> et concurrence au nb de <span className="hashtag">clics</span> polluent le paysage ? Réponses le 16 avril lors du 1er rdv de <span className="mention">@ScPoResearch</span> ✍ <Link to="/">https://bit.ly/2UQmrZI</Link>
+					</h2>			
+					<ul className="details">
+						<li className="account">@medialab_ScPo</li>
+					</ul>
+				</article>
+
+				<article data-type="git" >
+					<p className="type">Git</p>
+					<p className="update">1 days ago</p>
+
+					<h2 data-level-1="title">Bröder & Walter</h2>
+					<h3 data-level-2="description">Never gonna give you up.</h3>
+					
+					<ul className="details">
+						<li className="language">JS / HTML / CSS </li>
+						<li className="contrib">10 contributeurs</li>
+						<li className="licence">MIT licence</li>
+					</ul>
+				</article>
+
+				<article data-type="git" >
+					<p className="type">Git</p>
+					<p className="update">1 days ago</p>
+
+					<h2 data-level-1="title">Tugh & Marie</h2>
+					<h3 data-level-2="description">Bim bam bom</h3>
+					
+					<ul className="details">
+						<li className="language">JS / HTML / CSS </li>
+						<li className="contrib">6 contributeurs</li>
+						<li className="licence">MIT licence</li>
+					</ul>
+				</article>
+				<article data-type="tweet" className="major" data-lenght="140">
+					<p className="type">Tweet</p>
+					<p className="update">4 days ago</p>
+					{/*<h2 data-level-1="title">Tic et Toc</h2>*/}
+					<h2 data-level-2="description">
+						Le médialab accueille @VBeaudouin pour parler de son travail de #cartographie du web de la grande guerre.
+					</h2>			
+					<ul className="details">
+						<li className="account">@medialab_ScPo</li>
+					</ul>
+				</article>
+
+				
+				<article></article>							
+						
+
 			</div>
-			
-		</section>
-		*/
+
+			</section>
+		
 		</>
   	);
 }
 
-export default Flux;
+//export default Flux;
+

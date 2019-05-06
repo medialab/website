d1584f6a8461afb9df51dbf0cfa27d65f73f1b4b
JulieBlanc <contact@julie-blanc.fr>
2019-05-06 15:39:52 +0200

    styling flux

--- site/src/components/home/fragments/Flux.js
+++ site/src/components/home/fragments/Flux.js
@@ -10,76 +10,81 @@ export default function flux() {
 
 			<h1>À chaud</h1>
 
-			<div id="flux-container" style={{ '--h-0': 2, '--h-1': 2, '--h-2': 1, '--h-3': 1}}>
-				{/*if type=git (bon modèle pour les liens) */}
-				<article data-type="git" >
-					<p className="type">Git</p>
-					<p className="update">Today</p>
+			<div id="flux-container">
 
-					<h2 data-level-1="title">Tic et Toc</h2>
-					<h3 data-level-2="description">Outils composé de 0 ou de 1, de $ ou de , enfin plein de machin pas si complexe...</h3>
-					
-					<ul className="details">
-						<li className="language">JS / HTML / CSS </li>
-						<li className="contrib">3 contributeurs</li>
-						<li className="licence">LGPL-3.0</li>
-					</ul>
+				<article className="git" data-type="git">			
+					<p className="type">Git</p>
+					<p className="date last-update"><span>Last update: </span>23/10/2018</p>
+					<div class="content content-git">
+						<h1 data-level-1="title">Tic et Toc</h1>
+						<p className="description">Outils composé de 0 ou de 1, de $ ou de , enfin plein de machin pas si complexe.</p>
+						<ul className="details">
+							<li className="language">Python</li>
+							<li className="contrib">3 contributeurs</li>
+							<li className="licence">LGPL-3.0</li>
+						</ul>
+					</div>
 				</article>
 
-				{/*if type=tweet (bon modèle pour les liens) */}
-				<article data-type="tweet" className="major" data-lenght="280">
+				<article className="item-tweet" data-type="tweet">			
 					<p className="type">Tweet</p>
-					<p className="update">Today</p>
-					{/*<h2 data-level-1="title">Tic et Toc</h2>*/}
-					<h2 data-level-2="description">
-						Faire vivre la #démocratie exige que nous ayons <span className="hashtag">#confiance</span> envers les <span className="hashtag">#medias</span>. Comment y parvenir dans un univers où <span className="hashtag">#trolls</span>, <span className="hashtag">#FakeNews</span> et concurrence au nb de <span className="hashtag">clics</span> polluent le paysage ? Réponses le 16 avril lors du 1er rdv de <span className="mention">@ScPoResearch</span> ✍ <Link to="/">https://bit.ly/2UQmrZI</Link>
-					</h2>			
-					<ul className="details">
-						<li className="account">@medialab_ScPo</li>
-					</ul>
+					<p className="date"><span>Publié le </span>23/10/2018</p>					
+					<div class="content content-tweet">
+						<div className="tweet-inside">
+						<p>
+							Faire vivre la #démocratie exige que nous ayons <span className="hashtag">#confiance</span> envers les <span className="hashtag">#medias</span>. Comment y parvenir dans un univers où <span className="hashtag">#trolls</span>, <span className="hashtag">#FakeNews</span> et concurrence au nb de <span className="hashtag">clics</span> polluent le paysage ? Réponses le 16 avril lors du 1er rdv de <span className="mention">@ScPoResearch</span> ✍ <Link to="/">https://bit.ly/2UQmrZI</Link>
+						</p>
+						</div>
+						<ul className="details">
+							<li className="language">@medialab_ScPo</li>
+						</ul>
+					</div>				
 				</article>
 
-				<article data-type="git" >
+
+				<article className="git" data-type="git">			
 					<p className="type">Git</p>
-					<p className="update">1 days ago</p>
-
-					<h2 data-level-1="title">Bröder & Walter</h2>
-					<h3 data-level-2="description">Never gonna give you up.</h3>
-					
-					<ul className="details">
-						<li className="language">JS / HTML / CSS </li>
-						<li className="contrib">10 contributeurs</li>
-						<li className="licence">MIT licence</li>
-					</ul>
+					<p className="date last-update"><span>Last update: </span>23/10/2018</p>
+					<div class="content content-git">
+						<h1 data-level-1="title">Bröder &amp; Walter</h1>
+						<p className="description">Never gonna give you up</p>
+						<ul className="details">
+							<li className="language">TypeScript</li>
+							<li className="contrib">10 contributeurs</li>
+							<li className="licence">MIT licence</li>
+						</ul>
+					</div>
 				</article>
 
-				<article data-type="git" >
+				<article className="git" data-type="git">			
 					<p className="type">Git</p>
-					<p className="update">1 days ago</p>
-
-					<h2 data-level-1="title">Tugh & Marie</h2>
-					<h3 data-level-2="description">Bim bam bom</h3>
-					
-					<ul className="details">
-						<li className="language">JS / HTML / CSS </li>
-						<li className="contrib">6 contributeurs</li>
-						<li className="licence">MIT licence</li>
-					</ul>
+					<p className="date last-update"><span>Last update: </span>23/10/2018</p>
+					<div class="content content-git">
+						<h1 data-level-1="title">Website</h1>
+						<p className="description">The lab's static website and its admin.</p>
+						<ul className="details">
+							<li className="language">Javascript</li>
+							<li className="contrib">10 contributeurs</li>
+							<li className="licence">MIT licence</li>
+						</ul>
+					</div>
 				</article>
-				<article data-type="tweet" className="major" data-lenght="140">
+
+
+				<article className="item-tweet" data-type="tweet">			
 					<p className="type">Tweet</p>
-					<p className="update">4 days ago</p>
-					{/*<h2 data-level-1="title">Tic et Toc</h2>*/}
-					<h2 data-level-2="description">
-						Le médialab accueille <span className="mention">@VBeaudouin</span> pour parler de son travail de #cartographie du web de la grande guerre.
-					</h2>			
-					<ul className="details">
-						<li className="account">@medialab_ScPo</li>
-					</ul>
+					<p className="date"><span>Publié le </span>23/10/2018</p>					
+					<div class="content content-tweet">
+						<div className="tweet-inside">
+							<p>Le médialab accueille <span className="mention">@VBeaudouin</span> pour parler de son travail de #cartographie du web de la grande guerre.</p>
+							<img src="/" />
+						</div>
+						<ul className="details">
+							<li className="language">@medialab_ScPo</li>
+						</ul>
+					</div>				
 				</article>
 
-				
-				<article></article>							
 						
 
 			</div>

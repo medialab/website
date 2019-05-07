3a67d3e66039933768ff6045367b00ab400a86f3
BenjmnG <Benjmng@gmail.com>
2019-05-07 12:55:50 +0200

    Flux : Fixed height container and scrollable overflow inside

--- site/src/components/home/fragments/Flux.js
+++ site/src/components/home/fragments/Flux.js
@@ -10,31 +10,32 @@ export default function flux({rdv, lang}) {
 		<>
 		
 		<section id="flux">
+			<div className="container">
+		        <input
+		          type="radio" name="flux_selection" id="flux_selection-agenda"
+		          className="flux_selection" hidden defaultChecked />
+		        <label className="flux_selection-label" htmlFor="flux_selection-agenda">
+		        	{lang === 'fr' ? 'Les rendez-vous ' : 'The agenda'}
+		        </label>
 
-	        <input
-	          type="radio" name="flux_selection" id="flux_selection-agenda"
-	          className="flux_selection" hidden defaultChecked />
-	        <label className="flux_selection-label" htmlFor="flux_selection-agenda">
-	        	{lang === 'fr' ? 'Les rendez-vous ' : 'The agenda'}
-	        </label>
+		        <input
+		          type="radio" name="flux_selection" id="flux_selection-git"
+		          className="flux_selection" hidden />
+		        <label className="flux_selection-label" htmlFor="flux_selection-git">
+		        	{lang === 'fr' ? 'Projets Git ' : 'Git project'}
+		        </label>
 
-	        <input
-	          type="radio" name="flux_selection" id="flux_selection-git"
-	          className="flux_selection" hidden />
-	        <label className="flux_selection-label" htmlFor="flux_selection-git">
-	        	{lang === 'fr' ? 'Projets Git ' : 'Git project'}
-	        </label>
+		        <input
+		          type="radio" name="flux_selection" id="flux_selection-tweet"
+		          className="flux_selection" hidden />
+		        <label className="flux_selection-label" htmlFor="flux_selection-tweet">
+		        	{lang === 'fr' ? 'Dernier tweets ' : 'Last tweets'}
+		        </label>
 
-	        <input
-	          type="radio" name="flux_selection" id="flux_selection-tweet"
-	          className="flux_selection" hidden />
-	        <label className="flux_selection-label" htmlFor="flux_selection-tweet">
-	        	{lang === 'fr' ? 'Dernier tweets ' : 'Last tweets'}
-	        </label>
-
-			<Agenda rdv={rdv} lang={lang} />
-			<Git lang={lang} />
-			<Tweet lang={lang} />
+				<Agenda rdv={rdv} lang={lang} />
+				<Git lang={lang} />
+				<Tweet lang={lang} />
+			</div>
 		</section>
 		
 		</>

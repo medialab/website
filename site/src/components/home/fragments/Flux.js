cd2a9da0fd0fc84a09a0d309ea486b194db09d20
BenjmnG <Benjmng@gmail.com>
2019-05-11 19:45:26 +0200

    Flux Label Sticky // Flux container for large screen

--- site/src/components/home/fragments/Flux.js
+++ site/src/components/home/fragments/Flux.js
@@ -14,23 +14,24 @@ export default function flux({rdv, lang}) {
 		        <input
 		          type="radio" name="flux_selection" id="flux_selection-agenda"
 		          className="flux_selection" hidden defaultChecked />
-		        <label className="flux_selection-label" htmlFor="flux_selection-agenda">
-		        	{lang === 'fr' ? 'Les rendez-vous ' : 'The agenda'}
-		        </label>
-
 		        <input
 		          type="radio" name="flux_selection" id="flux_selection-git"
 		          className="flux_selection" hidden />
-		        <label className="flux_selection-label" htmlFor="flux_selection-git">
-		        	{lang === 'fr' ? 'Projets Git ' : 'Git project'}
-		        </label>
-
 		        <input
 		          type="radio" name="flux_selection" id="flux_selection-tweet"
 		          className="flux_selection" hidden />
-		        <label className="flux_selection-label" htmlFor="flux_selection-tweet">
-		        	{lang === 'fr' ? 'Dernier tweets ' : 'Last tweets'}
-		        </label>
+
+		        <div className="flux_selection_container">
+			        <label className="flux_selection-label for_agenda" htmlFor="flux_selection-agenda">
+			        	{lang === 'fr' ? 'Les rendez-vous ' : 'The agenda'}
+			        </label>
+			        <label className="flux_selection-label for_git" htmlFor="flux_selection-git">
+			        	{lang === 'fr' ? 'Projets Git ' : 'Git project'}
+			        </label>		          
+			        <label className="flux_selection-label for_tweet" htmlFor="flux_selection-tweet">
+			        	{lang === 'fr' ? 'Dernier tweets ' : 'Last tweets'}
+			        </label>
+			    </div>
 
 				<Agenda rdv={rdv} lang={lang} />
 				<Git lang={lang} />

aca3e09cd0f854bed846e99d4510370ea27effa8
Yomguithereal <guillaumeplique@gmail.com>
2019-05-21 18:29:25 +0200

    Adding npm run flux

--- scripts/flux-github.js
+++ scripts/flux-github.js
@@ -1,5 +1,6 @@
 const config = require('config');
 const path = require('path');
+const fs = require('fs');
 const {retrieveGithubFluxData} = require('../api/flux.js');
 
 const people = require(path.join(__dirname, '..', config.get('data'), 'people.json')).people;
@@ -8,5 +9,8 @@ retrieveGithubFluxData(people, (err, data) => {
   if (err)
     return console.error(err);
 
-  console.log(JSON.stringify(data, null, 2));
+  fs.writeFileSync(
+    path.join(__dirname, '..', config.get('data'), 'github.json'),
+    JSON.stringify(data, null, 2)
+  );
 });

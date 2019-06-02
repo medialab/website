a2482423466a8e0326720d6da6e7739b2ec044ec
BenjmnG <Benjmng@gmail.com>
2019-06-02 17:52:23 +0200

    Home : Flux et Now sont desormais réduits par defaut en version Mobile

--- site/src/components/home/fragments/flux/Agenda.js
+++ site/src/components/home/fragments/flux/Agenda.js
@@ -12,37 +12,43 @@ export default function Agenda({rdv, lang}) {
 	return (
   <>
     <section id="agenda" /* style={nbRdv} */>
+    <>
+    <h1>{ lang === "fr" ? "Rendez-vous" : "Meeting"}</h1>
+    <input
+    type="radio" name="radio_flux" id="radio_agenda"
+    hidden />
+    <label className="responsive-flux" htmlFor="radio_agenda">
+      { lang === "fr" ? "Rendez-vous" : "Meeting"}
+    </label> 
+      <>
 
-    <h1>Rendez-vous</h1>
-          <>
+        {rdv.map((event, i) =>
+          (<React.Fragment key={i}>
 
-            {rdv.map((event, i) =>
-              (<React.Fragment key={i}>
+            <article>
+            <Link to={event.permalink[lang]}>
 
-                <article>
-                <Link to={event.permalink[lang]}>
-
-                  <aside className="divers">
-                    <p className="label" data-icon="news">{ event.label && (lang === 'fr' ? event.label.fr : event.label.en) }</p>
-                    <DateNews startDate={event.startDate} endDate={event.endDate} lang={lang} />
-                    {event.isInternal && <p className="internal" aria-label="evenement interne au medialab" title={lang === 'fr' ? "Cet évenement est organisé par le Medialab" : "The event is hosted by Medialab"} >⌂</p>}
-                  </aside>
+              <aside className="divers">
+                <p className="label" data-icon="news">{ event.label && (lang === 'fr' ? event.label.fr : event.label.en) }</p>
+                <DateNews startDate={event.startDate} endDate={event.endDate} lang={lang} />
+                {event.isInternal && <p className="internal" aria-label="evenement interne au medialab" title={lang === 'fr' ? "Cet évenement est organisé par le Medialab" : "The event is hosted by Medialab"} >⌂</p>}
+              </aside>
 
 
-                  <h1 data-level-1="title">{lang === 'fr' ? event.title.fr : event.title.en }</h1>
+              <h1 data-level-1="title">{lang === 'fr' ? event.title.fr : event.title.en }</h1>
 
-                  <aside className="details">
-                    <TimeNews startDate={event.startDate} endDate={event.endDate} />
-                    { event.place && <p className="place">{event.place}</p> }
-                  </aside>
+              <aside className="details">
+                <TimeNews startDate={event.startDate} endDate={event.endDate} />
+                { event.place && <p className="place">{event.place}</p> }
+              </aside>
 
-                </Link>
-                </article>
+            </Link>
+            </article>
 
-              </React.Fragment>)
-						)}
-          </>
-
+          </React.Fragment>)
+				)}
+      </>
+    </>
     </section>
   </>
   	);

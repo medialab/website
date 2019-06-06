9ebb7d9ba7d16f7bc8130cb5d605f6b3482a6a41
jeremy richard <jeremy.richard@sciencespo.fr>
2019-06-06 11:52:12 +0200

    :whale: nginx DNS cache issue on api upgrade (502 Bad Gateway)

--- admin/docker-cmd.sh
+++ admin/docker-cmd.sh
@@ -1,5 +1,8 @@
 # Templating the NGINX conf
-envsubst '\$API_HOST \$API_PORT \$STATIC_HOST \$STATIC_PORT' < /etc/nginx/conf.d/docker.template > /etc/nginx/conf.d/default.conf
+
+export NS=$(cat /etc/resolv.conf |grep nameserver|awk -F" " '{print $2}')
+
+envsubst '\$NS \$API_HOST \$API_PORT \$STATIC_HOST \$STATIC_PORT' < /etc/nginx/conf.d/docker.template > /etc/nginx/conf.d/default.conf
 
 # No daemon
 nginx -g 'daemon off;'

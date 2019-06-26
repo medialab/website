72d4da072bd5f39a68066bd48f2a9f80e84ee25b
Paul Girard <paul.girard@sciencespo.fr>
2019-06-26 14:12:42 +0200

    fixes #327

--- site/src/components/home/fragments/Slideshow.js
+++ site/src/components/home/fragments/Slideshow.js
@@ -6,55 +6,53 @@ import {IsModel} from '../../helpers/helpers.js';
 import DateNews from '../../helpers/DateNews.js';
 import TimeNews from '../../helpers/TimeNews.js';
 import {format as formatDate, getYear, parseISO} from 'date-fns';
+import { runInThisContext } from 'vm';
+import {ellipse} from '../../../../utils.js';
 
 // Ici nous composons l'ensemble du caroussel
 const Slideshow = ({slider, lang}) => {
 
-  const otherLang = lang === 'fr' ? 'en' : 'fr';
+const otherLang = lang === 'fr' ? 'en' : 'fr';
 
-	let a, z;
-	a = 3;
-	z = 1;
 
- 	function IsIndex (index) {
- 		if (index === 0) {
- 			a = '3';
- 			z = '2';
- 		}
- 		if (index === 1) {
- 			a = '1';
- 			z = '3';
- 		}
-  		if (index === 2) {
- 			a = '2';
- 			z = '1';
- 		}
- 	}
+let a, z;
+a = 3;
+z = 1;
+
+function IsIndex (index) {
+  if (index === 0) {
+    a = '3';
+    z = '2';
+  }
+  if (index === 1) {
+    a = '1';
+    z = '3';
+  }
+    if (index === 2) {
+    a = '2';
+    z = '1';
+  }
+}
 
 	return (
   <section className="slideshow" id="slideshow" data-nbr-item="3">
     <div className="slideshow-container">
+      {/* Bullet by default*/}
+      <input
+        type="radio" name="slide-bullet" id="slide-bullet-0"
+        className="slideshow-bullet" hidden defaultChecked />
+      <label className="" htmlFor="slide-bullet-0" aria-hidden="true">
+      </label>
+      {slider.map((slide, index) =>
+        (<React.Fragment key={index}>
+          {IsIndex(index)}
+          <input
+            type="radio" name="slide-bullet" id={`slide-bullet-${index + 1}`}
+            className="slideshow-bullet" hidden />
+        </React.Fragment>)
+        )}
 
-
-
-        {/* Bullet by default*/}
-        <input
-          type="radio" name="slide-bullet" id="slide-bullet-0"
-          className="slideshow-bullet" hidden defaultChecked />
-        <label className="" htmlFor="slide-bullet-0" aria-hidden="true">
-        </label>
-        {slider.map((slide, index) =>
-          (<React.Fragment key={index}>
-            {IsIndex(index)}
-            <input
-              type="radio" name="slide-bullet" id={`slide-bullet-${index + 1}`}
-              className="slideshow-bullet" hidden />
-          </React.Fragment>)
-          )}
-
-
-
-        <div className="slideshow-inner">
+      <div className="slideshow-inner">
         {slider.map((slide, index) =>
           (<React.Fragment key={index}>
             {/* Content */}
@@ -80,7 +78,7 @@ const Slideshow = ({slider, lang}) => {
                         </aside>
                         <h1 data-level-1="baseline" >{slide.data.baseline[lang] || slide.data.baseline[otherLang]}</h1>
                         <p className="accroche">
-                          {slide.data.description && (slide.data.description[lang] || slide.data.description[otherLang])}
+                          {slide.data.description && ellipse(slide.data.description[lang] || slide.data.description[otherLang], 175)}
                         </p>
                       </>
                     )}
@@ -93,7 +91,7 @@ const Slideshow = ({slider, lang}) => {
                           <p className="subtype-production"><span>{slide.data.typeLabel[lang]}</span></p>
                           <p className="date-production">{slide.data.date}</p>
                         </aside>
-                        <h1 data-level-1="title">{slide.data.title[lang] || slide.data.title[otherLang]}</h1>
+                        <h1 data-level-1="title">{ellipse(slide.data.title[lang] || slide.data.title[otherLang], 100)}</h1>
                         <h2 data-level-1="authors" className="authors">{slide.data.authors}</h2>
                       </>
                     )}
@@ -109,10 +107,10 @@ const Slideshow = ({slider, lang}) => {
                           <DateNews startDate={slide.data.startDate} endDate={slide.data.endDate} lang={lang} />
                           <TimeNews startDate={slide.data.startDate} endDate={slide.data.endDate} />
                         </div>
-                        <h1 data-level-1="baseline" >{slide.data.title[lang] || slide.data.title[otherLang]}</h1>
+                        <h1 data-level-1="baseline" >{ellipse(slide.data.title[lang] || slide.data.title[otherLang], 80)}</h1>
 
                         <p className="accroche">
-                          {slide.data.description[lang] || slide.data.description[otherLang]}
+                          {ellipse(slide.data.description[lang] || slide.data.description[otherLang], 175)}
                         </p>
                       </>
                     )}
@@ -123,28 +121,28 @@ const Slideshow = ({slider, lang}) => {
                 </div>
               </article>
             </div>
+        </React.Fragment>)
+        )}
+      </div>
+
+      {/* bullet pour controler le caroussel*/}
+      <nav className="bullets-slide">
+        <label className="slideshow-bullet-label" data-slide="1" htmlFor="slide-bullet-1" aria-label={lang === "fr" ? "Voir la première page" : "See first slide" }></label>
+        <label className="slideshow-bullet-label" data-slide="2" htmlFor="slide-bullet-2" aria-label={lang === "fr" ? "Voir la deuxième page" : "See second slide" }></label>
+        <label className="slideshow-bullet-label" data-slide="3" htmlFor="slide-bullet-3" aria-label={lang === "fr" ? "Voir la troisème page" : "See third slide" }></label>
+      </nav>
+
+      {/* label pour controler le caroussel*/}
+      <nav className="slideshow-controls" aria-hidden="true">
+        {slider.map((slide, index) =>
+          (<React.Fragment key={index}>
+              {IsIndex(index)}
+              <label htmlFor={`slide-bullet-${a}`} className="slide_controls slide_controls-previous" alt={lang === 'fr' ? 'Aller à la slide ' + a : 'Go to slide ' + a }></label>
+              <label htmlFor={`slide-bullet-${z}`} className="slide_controls slide_controls-next" alt={lang === 'fr' ? 'Aller à la slide ' + z : 'Go to slide ' + z }></label>
           </React.Fragment>)
-    			)}
-        </div>
-
-        {/* bullet pour controler le caroussel*/}
-        <nav className="bullets-slide">
-          <label className="slideshow-bullet-label" data-slide="1" htmlFor="slide-bullet-1" aria-label={lang === "fr" ? "Voir la première page" : "See first slide" }></label>
-          <label className="slideshow-bullet-label" data-slide="2" htmlFor="slide-bullet-2" aria-label={lang === "fr" ? "Voir la deuxième page" : "See second slide" }></label>
-          <label className="slideshow-bullet-label" data-slide="3" htmlFor="slide-bullet-3" aria-label={lang === "fr" ? "Voir la troisème page" : "See third slide" }></label>
-        </nav>
-
-        {/* label pour controler le caroussel*/}
-        <nav className="slideshow-controls" aria-hidden="true">
-          {slider.map((slide, index) =>
-            (<React.Fragment key={index}>
-                {IsIndex(index)}
-                <label htmlFor={`slide-bullet-${a}`} className="slide_controls slide_controls-previous" alt={lang === 'fr' ? 'Aller à la slide ' + a : 'Go to slide ' + a }></label>
-                <label htmlFor={`slide-bullet-${z}`} className="slide_controls slide_controls-next" alt={lang === 'fr' ? 'Aller à la slide ' + z : 'Go to slide ' + z }></label>
-            </React.Fragment>)
-          )}
-          <span className="controls" aria-hidden="true"></span>
-        </nav>
+        )}
+        <span className="controls" aria-hidden="true"></span>
+      </nav>
     </div>
     <hr />
   </section>

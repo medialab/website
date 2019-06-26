ce851cb8503bb0cc8abd936ebbc9734b6372a4ce
Paul Girard <paul.girard@sciencespo.fr>
2019-06-26 16:16:58 +0200

    controlling slides number relates #326

--- site/src/components/home/fragments/Slideshow.js
+++ site/src/components/home/fragments/Slideshow.js
@@ -14,15 +14,24 @@ const Slideshow = ({slider, lang}) => {
 
 const otherLang = lang === 'fr' ? 'en' : 'fr';
 
+  let slides = slider;
+  // limiting number of slide to 5 maximum
+  if (slider.length > 5)
+    slides = slider.slice(0, 5);
+  else
+    // duplicate if less than 3
+    if (slider.length === 2)
+      slides = slider.concat(slider);
+
 	return (
-  <section className="slideshow" id="slideshow" data-nbr-item={slider.length}>
+  <section className="slideshow" id="slideshow" data-nbr-item={slides.length}>
     <div className="slideshow-container">
       {/* Bullet by default*/}
       <input
         type="radio" name="slide-bullet" id="slide-bullet-0"
         className="slideshow-bullet" hidden defaultChecked />
       <label className="" htmlFor="slide-bullet-0" aria-hidden="true" />
-      {slider.map((slide, index) =>
+      {slides.length > 1 && slides.map((slide, index) =>
         (<React.Fragment key={index}>
           <input
             type="radio" name="slide-bullet" id={`slide-bullet-${index + 1}`}
@@ -31,7 +40,7 @@ const otherLang = lang === 'fr' ? 'en' : 'fr';
         )}
 
       <div className="slideshow-inner">
-        {slider.map((slide, index) =>
+        {slides.map((slide, index) =>
           (<React.Fragment key={index}>
             {/* Content */}
             <div className="slideshow-item" data-item={ index + 1 } aria-label={lang === "fr" ? "Pages misent en avant" : "Spotlighted pages" }>
@@ -99,31 +108,34 @@ const otherLang = lang === 'fr' ? 'en' : 'fr';
                 </div>
               </article>
             </div>
-        </React.Fragment>)
+          </React.Fragment>)
         )}
       </div>
 
       {/* bullet pour controler le caroussel*/}
       <nav className="bullets-slide">
-        { slider.map((slide, index) => {
+        { slides.length > 1 && slides.map((slide, index) => {
             return <label key={index + 1} className="slideshow-bullet-label" data-slide={index + 1} htmlFor={`slide-bullet-${index + 1}`} aria-label={`slide ${index + 1}`} />
           })}
       </nav>
 
       {/* label pour controler le caroussel*/}
-      <nav className="slideshow-controls" aria-hidden="true">
-        {slider.map((slide, index) =>
-          (<React.Fragment key={index}>
-            <label htmlFor={`slide-bullet-${index === 0 ? slider.length : index}`} className="slide_controls slide_controls-previous" alt={`${(lang === 'fr' ? 'Aller à la slide' : 'Go to slide')} ${index === 0 ? slider.length : index}`} />
-            <label htmlFor={`slide-bullet-${(index === slider.length - 1) ? 1 : index + 2}`} className="slide_controls slide_controls-next" alt={`${(lang === 'fr' ? 'Aller à la slide' : 'Go to slide')} ${(index === slider.length - 1) ? 1 : index + 2}`} />
-          </React.Fragment>)
-        )}
+      {slides.length > 1 && <nav className="slideshow-controls" aria-hidden="true">
+        {
+          slides.map((slide, index) =>
+            (<React.Fragment key={index}>
+              <label htmlFor={`slide-bullet-${index === 0 ? slides.length : index}`} className="slide_controls slide_controls-previous" alt={`${(lang === 'fr' ? 'Aller à la slide' : 'Go to slide')} ${index === 0 ? slides.length : index}`} />
+              <label htmlFor={`slide-bullet-${(index === slides.length - 1) ? 1 : index + 2}`} className="slide_controls slide_controls-next" alt={`${(lang === 'fr' ? 'Aller à la slide' : 'Go to slide')} ${(index === slides.length - 1) ? 1 : index + 2}`} />
+            </React.Fragment>)
+          )
+        }
         <span className="controls" aria-hidden="true" />
       </nav>
+      }
     </div>
     <hr />
   </section>
-  	);
+  );
 };
 
 export default Slideshow;

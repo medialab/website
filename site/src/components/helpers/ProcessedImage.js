ed196472f74177890c9c00ae185a1732e4937247
Yomguithereal <guillaumeplique@gmail.com>
2019-06-26 16:57:34 +0200

    Essai de zonage des images processed Related to #279

--- site/src/components/helpers/ProcessedImage.js
+++ site/src/components/helpers/ProcessedImage.js
@@ -1,28 +1,49 @@
 import React from 'react';
 
+const BLANK = '\u00A0';
+
 const ROWS = {
   small: 60,
   medium: 120,
   large: 240
 };
 
-
-
+const MARGIN = 1;
 
 function makeid(length) {
-  var text = "";
-  var possible = "░▒▓";
+  let text = '';
+  const possible = '░▒▓';
 
-  for (var i = 0; i < length; i++)
+  for (let i = 0; i < length; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
 }
 
+function findFirstColoredBlock(row) {
+  for (let i = 0; i < row.length; i++) {
+    if (row[i] !== BLANK)
+      return i;
+  }
+
+  return row.length;
+}
+
+function findAnchor(image, rows, b) {
+  const firstColoredBlock = Array.from(new Array(b), () => rows);
+
+  for (let i = 0; i < 10; i++) {
+    const offset = i * rows;
+
+    firstColoredBlock[i] = findFirstColoredBlock(image.slice(offset , offset + rows));
+  }
+
+  return Math.min.apply(null, firstColoredBlock);
+}
+
 const PLACEHOLDER_CHARACTERS = makeid(12);
 
 export default function ProcessedImage({image, size}) {
-
   const needPlaceholder = !image;
 
   const rows = ROWS[size];
@@ -31,14 +52,16 @@ export default function ProcessedImage({image, size}) {
 
   const b = (length / rows) | 0;
 
+  const anchor = Math.max(0, image ? findAnchor(image, rows, b) - MARGIN: 0);
+
   return (
     <>
       {Array.from(new Array(b), (_, i) => {
         const offset = i * rows;
 
         const row = needPlaceholder ?
-          PLACEHOLDER_CHARACTERS.repeat( rows / PLACEHOLDER_CHARACTERS.length )  :
-          image.slice(offset , offset + rows);
+          PLACEHOLDER_CHARACTERS.repeat(rows / PLACEHOLDER_CHARACTERS.length)  :
+          image.slice(offset + anchor, offset + rows);
 
         return (
           <pre key={i}>{row}</pre>

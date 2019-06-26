08d564b2172112a0d59a1b3ca867cdfa9def6bae
Paul Girard <paul.girard@sciencespo.fr>
2019-06-26 16:53:29 +0200

    fixes #251

--- site/src/components/pages_object/fragments/ToggleLang.js
+++ site/src/components/pages_object/fragments/ToggleLang.js
@@ -2,24 +2,34 @@ import React from 'react';
 
 export default function Footer ({lang, content}) {
 
-  let fr, en;
+  const enoughContentLength = 300;
 
-  fr = '⚐ Cette article existe aussi en anglais. ';
-  en = '⚐ This article is also in french. ';
+  const isEnEnough = content.en && content.en.length >= enoughContentLength;
+  const isFrEnough = content.fr && content.fr.length >= enoughContentLength;
+  const showSwitchToEn = isEnEnough && !isFrEnough;
+  const showSwitchToFr = isFrEnough && !isEnEnough;
+  const message = {
+    fr: '⚐ Cet article est plus complet en anglais. ',
+    en: '⚐ This article could be more substantial in french. '
+  };
+  // let fr, en;
 
-  if (content.fr.length < content.en.length) {
-    fr = '⚐ Cet article pourrait être plus complet en anglais. ';
-      if (content.fr.length < 50) {
-        fr = '⚐ Cet article est plus complet en anglais. ';
-      }
-  }
+  // fr = '⚐ Cette article existe aussi en anglais. ';
+  // en = '⚐ This article is also in french. ';
 
-  if (content.en.length < content.fr.length) {
-    en = '⚐ This article could be more substantial in french. ';
-      if (content.en.length < 50) {
-        en = '⚐ This article could be more substantial in french. ';
-      }
-  }
+  // if (content.fr.length < content.en.length) {
+  //   fr = '⚐ Cet article pourrait être plus complet en anglais. ';
+  //     if (content.fr.length < 50) {
+  //       fr = '⚐ Cet article est plus complet en anglais. ';
+  //     }
+  // }
+
+  // if (content.en.length < content.fr.length) {
+  //   en = '⚐ This article could be more substantial in french. ';
+  //     if (content.en.length < 50) {
+  //       en = '⚐ This article could be more substantial in french. ';
+  //     }
+  // }
 
   return (
     <>
@@ -27,19 +37,24 @@ export default function Footer ({lang, content}) {
         type="radio" id="fr-to-en" name="toggle-lang"
         value="fr-to-en" className="fr" defaultChecked={lang === 'en'}
         hidden />
-      <label htmlFor="fr-to-en" className="fr toggle-lang" href="#main-objet" aria-label={lang === "fr" ? fr : en }>
-        {fr}
-        <span aria-hidden="true">Cliquer ici pour le voir.</span>
-      </label>
+      { showSwitchToEn && 
+        <label htmlFor="fr-to-en" className="fr toggle-lang" href="#main-objet" aria-label={message.fr}>
+          {message.fr}
+          <span aria-hidden="true">Cliquer ici pour le voir.</span>
+        </label>
+      }
 
       <input
         type="radio" id="en-to-fr" name="toggle-lang"
         value="en-to-fr" className="en" defaultChecked={lang === 'fr'}
         hidden />
-      <label htmlFor="en-to-fr" className="en toggle-lang" aria-label={lang === "fr" ? fr : en }>
-        {en}
+      {showSwitchToFr &&
+      <label htmlFor="en-to-fr" className="en toggle-lang" aria-label={message.en}>
+        {message.en}
         <span aria-hidden="true">Click here to see it.</span>
       </label>
+      }
+      
     </>
   );
 }

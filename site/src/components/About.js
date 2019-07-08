2569e977460eea351d37fce93717e85c6b2664d6
AmelieVRL <37452370+AmelieVRL@users.noreply.github.com>
2019-07-08 17:28:49 +0200

    Update About.js

--- site/src/components/About.js
+++ site/src/components/About.js
@@ -69,50 +69,46 @@ export default function About({lang}) {
   else 
     // ***************************    ENGLISH VERSION
     return (
-      <>
-        <main id="main" aria-labelledby="page-title" role="main">
+      <>        <main id="main" aria-labelledby="page-title" role="main">
         <nav className="main-nav" id="nav-inside-article" role="navigation">
           <ul>
-            <li className="nav-inside-item" data-type="main"><a href="#main" title="Aller à la section" > Aller en haut</a></li>
-            <li className="nav-inside-item"><a href="#research" title="Aller à la section" >La recherche au médialab</a></li>
-            <li className="nav-inside-item"><a href="#team" title="Aller à la section" >Equipe</a></li>
-            <li className="nav-inside-item"><a href="#activities" title="Aller à la section" >Activités</a></li>
+            <li className="nav-inside-item" data-type="main"><a href="#main" title="Aller à la section" > Go to the Top</a></li>
+            <li className="nav-inside-item"><a href="#research" title="Aller à la section" >Research at the médialab</a></li>
+            <li className="nav-inside-item"><a href="#team" title="Aller à la section" >Team</a></li>
+            <li className="nav-inside-item"><a href="#activities" title="Aller à la section" >Activities</a></li>
             <li className="nav-inside-item"><a href="#productions" title="Aller à la section" >Productions</a></li>
           </ul>
         </nav>
           <article id="about-contenu" className="main-container">
             <hgroup>
-              <h1 id="page-title">Le médialab</h1>
-              <h2>ANGLAIS PLEASE</h2>
-            </hgroup>
+              <h1 id="page-title">The médialab</h1>
+              <h2>The Médialab, an interdisciplinary research laboratory comprised of sociologists, engineers and designers, conducts thematic and methodological research to investigate the role of digital technology in our societies.</h2>            </hgroup>
             <div className="about-contenu">
 
-              <h3 id="research">La recherche au médialab</h3>
-
-              <p>Central dans les réflexions du médialab depuis sa <span className="totip">création<span className="tip">BlaBla</span></span>, le numérique y est envisagé tant comme un objet d’étude que comme un moyen de recherche en sciences sociales. Les transformations engendrées par les technologies numériques sont sources de rapports complexes que les travaux du médialab visent à analyser et à représenter.
-              Le changement de paradigme dans notre rapport au monde induit par ces nouvelles articulations  centrent les recherches autour de grands défis contemporains qui se répartissent en quatre thèmes : “espace public numérique”, “transition énergétique”, “futurs technologiques”, et “Quantitatives Cultural studies”.</p>
-
-              <p>Chaque projet de recherche développe une approche pluridisciplinaire qui mobilise des compétences variées. Irrigués par les sciences sociales et les STS, ces projets se nourrissent au besoin de l'ingénierie informatique, ancrée dans l’ADN du médialab, pour affiner les enquêtes utilisant le web, mais aussi des nouvelles formes de mises en situation explorées par le design. </p>
-              <p>--
-              Faire le lien avec pédagogie:
-              Un lien fort avec les activités de pédagogie
-              Les nouvelles formes de pédagogie entrent pleinement
-              --</p>
-
-              <h3 id="team">Equipe</h3>
-              <p>Composée d’hommes et de femmes aux compétences complémentaires, l’équipe du médialab est plurielle. Membres ou associés au laboratoire, ces profils académiques, techniques, en design, ou encore en pédagogie se combinent et travaillent ensemble pour aviver la force des activités du laboratoire.</p>
-              <p><Link to="/people" className="GoTo">Découvrir les membres de l'équipe</Link></p>
-
-              <h3 id="activities">Activités</h3>
-              <p>A la fois objet de recherche et outils au service des projets, les méthodes numériques sont au coeur des activités du médialab et contribuent à nourrir la réflexion sur les enjeux contemporains tant en sociologie qu’en STS. </p>
-              <p>Organisées entre recherche, enseignement et méthodes, ces activités étudient notamment la
-              montée des populismes, les futurs écologiques ou encore de l’histoire de l’art numérique.</p>
-              <p><Link to="/activities" className="GoTo">Voir la liste des activités</Link></p>
-
-              <h3 id="productions">Productions</h3>
-              <p>Issues des dynamiques de recherche du laboratoire combinant méthode, analyse et théorie, les productions du médialab constituent un panorama hétéroclite. Aux traditionnelles publications académiques s’ajoute un ensemble de réalisations techniques qui répondent à des problèmes de recherche récurrents. Récemment, les sites web et réalisations en situation se sont développés comme de nouvelles formes pour rendre compte des activités du laboratoire.</p>
-              <p><Link to="/productions" className="GoTo">Voir la liste des productions</Link></p>
-
+              <h3 id="research">Research at the médialab</h3>
+              
+              <p>Digital technology is transforming our societies on the whole and the production of scientific knowledge in particular. The digital turn, central to the approach developed at the Médialab, calls for the articulation of three research approaches, drawn from the social sciences, engineering, and design. Through the multitude of data that it generates, the digital turn contributes to deepening our understanding of different social worlds. At the same time, it offers investigative tools that spawn new forms of knowledge production through the modelling, visualization and interactive exploration of data. Finally, it fosters experimentation with the public and students by encouraging new forms of reflexivity and new ways of sharing research results.</p>              
+              
+              <p>These research approaches are developed jointly around four main themes: :
+                  <li><b>the digital public space</b>, mapping the transformations of journalistic and political spaces under the impact of digital technology. Research in this field focuses on new information channels, the shaping of the public agenda, and parliamentary activities;</li> 
+                  <li><b>the environmental turn</b>, using digital methods to explore emerging issues at the crossroads between science, politics and societal expectations;</li>
+                  <li><b>technological futures</b>, adopting a Science & Technology Studies (STS) perspective to study how new artificial intelligence computation technology is penetrating our societies, investigating both the design of this technology and the effects of its increasing penetration into social realms;</li>
+                  <li><b>Quantitative Cultural studies</b> which, drawing on data from museums and cultural institutions, endeavour to explore our societies’ new forms of organization of culture with respect to works and their circulation. This field of study is also concerned with cultural festivals and the production of scientific knowledge about Russia.</li>
+               </p>
+  
+              <p>Every research project carried out at the Médialab follows a multidisciplinary approach mobilizing a variety of competences. These projects, informed by social science and STS, apply computer engineering to refine investigation by using the Web as a field of inquiry and data science as an analytical method. They also employ new forms of situated testing and exploration of the research questions raised by design.
+The Médialab is historically committed to studying the production and transmission of knowledge, and has experimented with new forms of teaching since its inception. This connection with students is evident in the courses taught by the members of the Medialab team, and is also reflected by the research programme it hosts on innovative teaching, fully informed by the laboratory's research practices.</p>
+      
+              <h3 id="team">Team</h3>
+              <p>The Médialab is a diverse research team, comprised of men and women with complementary skills. As members or partners of the laboratory, these academic, technical, design and teaching experts join forces and work together to develop research that draws on this diversity.</p>
+              <Link to="/people" className="GoTo">Découvrir les membres de l'équipe</Link></p>
+              <h3 id="research">Activities</h3>
+              <p>The Médialab’s activities articulate both research and teaching. In particular, they explore the use of digital methods to address contemporary issues in sociology and STS. These methods are also central to the laboratory’s teaching, for example by putting students in research situations. This fosters highly enriching reflection on our research processes. </p>
+              <Link to="/activities" className="GoTo">Voir la liste des activités</Link></p>
+              <h3 id="production">Productions</h3>
+              <p>In addition to traditional academic publications, "Web publications" also disseminate the laboratory's activities through an interactive medium that opens up new ways of representing and visually exploring findings. Situations – exhibitions, workshops, simulations, etc. – allow for the public’s involvement in the research process and the testing of hypotheses against the reality on the ground</p>
+              <Link to="/productions" className="GoTo">Voir la liste des productions</Link></p>
+      
             </div>
             {/*  <aside id="inshort">
               <h3>Side chose</h3>

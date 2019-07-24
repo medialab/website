e5a8ff09b8ce989d9d48025f726c9771586fd8fd
Robin de Mourat <robin.demourat@gmail.com>
2019-07-24 10:54:55 +0200

    fix bad anchors in about

--- site/src/components/About.js
+++ site/src/components/About.js
@@ -45,17 +45,17 @@ export default function About({lang}) {
               <p>Chacun des projets de recherche menés au médialab se développe dans une approche pluridisciplinaire mobilisant des compétences variées. Irrigués par les sciences sociales et les STS, ces projets se nourrissent de l'ingénierie informatique, pour affiner les enquêtes en utilisant le web comme terrain d’enquête ou la science des données comme méthode d’analyse. Ils se saisissent également de nouvelles formes de mises en situation et d’exploration des questions de recherche proposées par le design.
               Historiquement attaché à la question de la production et de la transmission des connaissances, le médialab expérimente depuis ses débuts de nouvelles formes d’interventions avec les étudiants. Tangible dans les cours dispensés par les membres de son équipe, ce lien se manifeste également par l’accueil d’un programme de recherche en pédagogies innovante qui s’ancre pleinement dans les pratiques de recherche du laboratoire.</p>
 
-              <h3 id="research">Equipe</h3>
+              <h3 id="team">Equipe</h3>
               <p>Composée d’hommes et de femmes aux compétences complémentaires, l’équipe du médialab est plurielle. Membres ou associés au laboratoire, ces profils académiques, techniques, en design, ou encore en pédagogie se combinent et travaillent ensemble pour développer une recherche se nourrissant de cette diversité.</p>
 
               <p><Link to="/people" className="GoTo">Découvrir les membres de l'équipe</Link></p>
 
-              <h3 id="research">Activités</h3>
+              <h3 id="activities">Activités</h3>
               <p>Les activités du médialab s’articulent entre recherche et enseignement. Elles approfondissent notamment l’usage des méthodes numériques pour répondre aux enjeux contemporains en sociologie et STS. Également mobilisées au coeur d’activités pédagogiques, par exemple en mettant les étudiants en situation d’enquête, elles bénéficient d’une réflexivité très enrichissante sur nos processus de recherche. </p>
 
               <p><Link to="/activities" className="GoTo">Voir la liste des activités</Link></p>
 
-              <h3 id="research">Productions</h3>
+              <h3 id="productions">Productions</h3>
               <p>Aux traditionnelles publications académiques s’ajoutent les “éditions web” qui projettent les activités du laboratoire dans un média interactif et ouvrent de nouveaux moyens de représentation et d’exploration visuelle des résultats. Les mises en situation -expositions, workshops, simulations, etc.- permettent d’engager des publics dans le processus de recherche et de confronter les hypothèses à la réalité du terrain.</p>
 
               <p><Link to="/productions" className="GoTo">Voir la liste des productions</Link></p>

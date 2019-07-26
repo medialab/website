6f47f798c32801befaf248eaad3959fb52e2aaf2
Paul Girard <paul.girard@sciencespo.fr>
2019-07-26 16:15:49 +0200

    static page integration

--- site/src/components/Legal.js
+++ site/src/components/Legal.js
@@ -10,24 +10,24 @@ export default function Legal({lang}) {
         <nav className="main-nav" id="nav-inside-article" role="navigation">
           <ul>
             <li className="nav-inside-item" data-type="main"><a href="#main" title="Aller à la section" > Aller en haut</a></li>
-            <li className="nav-inside-item" data-type="main"><a href="#directeur" title="Aller à la section" > Directeur de la publication</a></li>
-            <li className="nav-inside-item" data-type="main"><a href="#host" title="Aller à la section" > Hébergement</a></li>
-            <li className="nav-inside-item" data-type="main"><a href="#PI" title="Aller à la section" > Propriété Industrielle et Intellectuelle</a></li>
-            <li className="nav-inside-item" data-type="main"><a href="hypertext" title="Aller à la section" > Les liens hypertextes</a></li>
-            <li className="nav-inside-item" data-type="main"><a href="#images" title="Aller à la section" > Images</a></li>
-            <li className="nav-inside-item" data-type="main"><a href="#IL" title="Aller à la section" > Inform et liberté</a></li>
+            <li className="nav-inside-item" ><a href="#directeur" title="Aller à la section" > Directeur de la publication</a></li>
+            <li className="nav-inside-item" ><a href="#host" title="Aller à la section" > Hébergement</a></li>
+            <li className="nav-inside-item" ><a href="#PI" title="Aller à la section" > Propriété Industrielle et Intellectuelle</a></li>
+            <li className="nav-inside-item" ><a href="#hypertext" title="Aller à la section" > Les liens hypertextes</a></li>
+            <li className="nav-inside-item" ><a href="#images" title="Aller à la section" > Images</a></li>
+            <li className="nav-inside-item" ><a href="#IL" title="Aller à la section" > Informatique et liberté</a></li>
            </ul>
         </nav>
           <article id="about-contenu" className="main-container">
             <hgroup>
               <h1 id="page-title">Mentions légales</h1>
-
-              <p><b>Le contenu de ce site internet est fourni par :</b>
-              Sciences Po
-              27 rue Saint-Guillaume
-              75337 Paris Cedex 07
-              Tel. : 01 45 49 50 50
-              Fax. : 01 42 22 31 26
+            </hgroup>
+            <div className="about-contenu">
+              <p><b>Le contenu de ce site internet est fourni par :</b><br/>
+              Sciences Po<br/>
+              27 rue Saint-Guillaume<br/>
+              75337 Paris Cedex 07<br/>
+              <nobr>Tel. : 01 45 49 50 50</nobr> <nobr>Fax. : 01 42 22 31 26</nobr><br/>  
               webmestre@sciencespo.fr</p>
 
               <h3 id="directeur">Directeur de la publication</h3>
@@ -49,16 +49,16 @@ export default function Legal({lang}) {
               <p>Nos pages web proposent également des liens vers d'autres sites pour lesquels nous ne sommes responsables ni de leur intégral respect aux normes d'ordre public et bonnes mœurs, d'une part, ni de leur politique de protection des données personnelles ou d'utilisation qui en seraient faites, d'autre part.
               En accédant à un autre site, par l'intermédiaire d'un lien hypertexte, vous acceptez que cet accès s'effectue à vos risques et périls. En conséquence, tout préjudice direct ou indirect résultant de votre accès à un autre site relié par un lien hypertexte ne peut engager la responsabilité de Sciences Po.</p>
 
-              <h3 id="image">Images</h3>
+              <h3 id="images">Images</h3>
               <p>Les images du site sont libres de droit, sauf mentions crontraires.</p>
 
               <h3 id="IL">Informatique et Libertés</h3>
-              Droit d'accès, modification suppression des données personnelles vous concernant :
-
-                  par courrier :  médialab, 27 rue Saint Guillaume 75337 Paris Cedex 07
-                  par courrier électronique : medialab[AT]sciencespo.fr
-              </hgroup>
-            </article>
+              <p>Droit d'accès, modification suppression des données personnelles vous concernant :<br/>
+                  par courrier :  médialab, 27 rue Saint Guillaume 75337 Paris Cedex 07<br/>
+                  par courrier électronique : medialab[AT]sciencespo.fr<br/>
+              </p>
+            </div>
+          </article>
       </main>
     );
   else
@@ -68,22 +68,23 @@ export default function Legal({lang}) {
         <nav className="main-nav" id="nav-inside-article" role="navigation">
           <ul>
             <li className="nav-inside-item" data-type="main"><a href="#main" title="Aller à la section" > Aller en haut</a></li>
-            <li className="nav-inside-item" data-type="main"><a href="#publisher" title="Aller à la section" > Publisher</a></li>
-            <li className="nav-inside-item" data-type="main"><a href="#host" title="Aller à la section" > Hosting</a></li>
-            <li className="nav-inside-item" data-type="main"><a href="#PI" title="Aller à la section" > Industrial and Intellectual Property</a></li>
-            <li className="nav-inside-item" data-type="main"><a href="hypertext" title="Aller à la section" > Hypertext Links</a></li>
+            <li className="nav-inside-item" ><a href="#publisher" title="Aller à la section" > Publisher</a></li>
+            <li className="nav-inside-item" ><a href="#host" title="Aller à la section" > Hosting</a></li>
+            <li className="nav-inside-item" ><a href="#PI" title="Aller à la section" > Industrial and Intellectual Property</a></li>
+            <li className="nav-inside-item" ><a href="#hypertext" title="Aller à la section" > Hypertext Links</a></li>
+            <li className="nav-inside-item" ><a href="#IL" title="Aller à la section" > Personal information</a></li>
            </ul>
         </nav>
           <article id="about-contenu" className="main-container">
             <hgroup>
               <h1 id="page-title">Legal Notice</h1>
-
-              <p><b>The contents of this site were supplied by :</b>
-              Sciences Po
-              27 rue Saint-Guillaume
-              75337 Paris Cedex 07
-              Tel. : 01 45 49 50 50
-              Fax. : 01 42 22 31 26
+            </hgroup>
+            <div className="about-contenu">
+              <p><b>The contents of this site were supplied by:</b><br/>
+              Sciences Po<br/>
+              27 rue Saint-Guillaume<br/>
+              75337 Paris Cedex 07<br/>
+              Tel. : 01 45 49 50 50 Fax. : 01 42 22 31 26<br/>
               webmestre@sciencespo.fr</p>
 
               <h3 id="publisher">Publisher</h3>
@@ -104,8 +105,13 @@ export default function Legal({lang}) {
               <h3 id="hypertext">Les liens hypertextes</h3>
               <p>Our pages also contain links to other sites for which we are in no manner responsible, neither regarding their adhesion to public order or good conduct, on the one hand, nor regarding their personal data privacy policies and use, on the other.
               By accessing an outside site, through a hypertext link, you accept that this access is carried out at your own risk. Therefore, Sciences Po will in no way be held responsible for any direct or indirect damages resulting from your access to an outside site through a hypertext link.</p>
-
-            </hgroup>
+              
+              <h3 id="IL">Personal information</h3>
+              <p>To learn about, get access to or ask for deletion of personal information we might have collected please contact:<br/>
+                  by mail :  médialab, 27 rue Saint Guillaume 75337 Paris Cedex 07<br/>
+                  by email : medialab[AT]sciencespo.fr<br/>
+              </p>
+            </div>
             {/*  <aside id="inshort">
               <h3>Side chose</h3>
               <p>L’autre originalité du médialab en terme d’organisation est de s’appuyer à parts égales sur trois métiers</p>

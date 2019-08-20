9b78789305573b2ff6aed39fba4da1a5151d2565
Yomguithereal <guillaumeplique@gmail.com>
2019-08-20 15:11:12 +0200

    More i18n

--- site/src/components/helpers/SearchInput.js
+++ site/src/components/helpers/SearchInput.js
@@ -1,52 +1,25 @@
 import React from 'react';
 import {Icons} from '../helpers/Icons.js';
 
-// function SearchInput(lang) {
+const i18n = {
+  fr: {
+    placeholder: 'Rechercher',
+    ariaLabel: 'Rechercher dans la page'
+  },
+  en: {
+    placeholder: 'Search',
+    ariaLabel: 'Search through page content'
+  }
+};
 
-//     let placeholder, arial;
-
-//     if (lang === 'fr') {
-//         placeholder = "Rechercher",
-//         arial = "Rechercher dans la page";
-//     }else{
-//         placeholder = "Search",
-//         arial = "Search through page content";
-//     }
-
-
-//     return (
-//         <div id="search">
-//              <input type="search" name="q" arial-label={arial} placeholder={placeholder} />
-//          </div>
-//     );
-
-// }
-
-// export default SearchInput;
-
-
-export function SearchInput(value) {
-
-    let placeholder, arial;
-
-    if (value.lang === 'fr') {
-        placeholder = 'Rechercher',
-        arial = 'Rechercher dans la page';
-    }
-else {
-        placeholder = 'Search',
-        arial = 'Search through page content';
-    }
-
-
-    return (
-      <div id="search">
-        <input
-          type="search" id="search-input" name="q"
-          arial-label={arial} placeholder={placeholder} />
-        <label htmlFor="search-input"><Icons icon="search" /></label>
-
-      </div>
-    );
+export function SearchInput({lang}) {
+  return (
+    <div id="search">
+      <input
+        type="search" id="search-input" name="q"
+        arial-label={i18n[lang].ariaLabel} placeholder={i18n[lang].placeholder} />
+      <label htmlFor="search-input"><Icons icon="search" /></label>
+    </div>
+  );
 }
 

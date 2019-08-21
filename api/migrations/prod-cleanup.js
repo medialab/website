52b894306df055706fad784292105d230b929600
Paul Girard <paul.girard@sciencespo.fr>
2019-08-21 12:12:12 +0200

    refacto now does not break all the data

--- api/migrations/prod-cleanup.js
+++ api/migrations/prod-cleanup.js
@@ -1,5 +1,4 @@
 const partition = require('lodash/partition');
-const flatten = require('lodash/flatten');
 
 const MODELS = [
   'activities',
@@ -11,44 +10,50 @@ const MODELS = [
 module.exports = function(req, dbs, next) {
   const dryRun = 'dryrun' in req.query;
 
-  const data = flatten(MODELS.map(plural => {
+  const data = {};
+  let deleted = [];
+
+  let stringifiedData = '';
+  MODELS.forEach(plural => {
     dbs[plural].read();
-
-    return dbs[plural].getState()[plural];
-  }));
-
-  // Finding things to delete
-  const [toDelete, toKeep] = partition(data, o => {
-    return (
-      (o.title && ((o.title.fr && o.title.fr.includes(' SUPPRIMER')) ||
-                  (o.title.en && o.title.en.includes(' SUPPRIMER'))) 
-      ) ||
-      (o.name && o.name.includes(' SUPPRIMER'))
-    );
+    data[plural] = dbs[plural].getState()[plural];
+    stringifiedData += JSON.stringify(data[plural]) + '\n';
   });
 
-  // Checking integrity
-  const stringifiedData = data.map(items => JSON.stringify(items)).join('\n');
+  MODELS.forEach(m => {
+      // Finding things to delete
+    const [toDelete, toKeep] = partition(data[m], o => {
+      return (
+        (o.title && ((o.title.fr && o.title.fr.includes(' SUPPRIMER')) ||
+                    (o.title.en && o.title.en.includes(' SUPPRIMER'))) 
+        ) ||
+        (o.name && o.name.includes(' SUPPRIMER'))
+      );
+    });
 
-  let error = null;
+    let error = null;
 
-  toDelete.some(news => {
-    const match = stringifiedData.match(news.id);
+    toDelete.some(o => {
+      const match = stringifiedData.match(o.id);
 
-    if (match && match.length > 1)
-      error = `${news.id} is still linked!`;
+      if (match && match.length > 1)
+        error = `${o.id} is still linked!`;
 
-    return error;
+      return error;
+    });
+
+    if (error)
+      return next(error);
+    deleted = deleted.concat(toDelete)
+
+    // Persisting
+    if (!dryRun) {
+      const newData = {};
+      newData[m] = toKeep;
+      dbs[m].setState(newData);
+      dbs[m].write();
+    }
   });
 
-  if (error)
-    return next(error);
-
-  // Persisting
-  if (!dryRun) {
-    dbs.news.setState({news: toKeep});
-    dbs.news.write();
-  }
-
-  return next(null, toDelete);
+  return next(null, deleted);
 };

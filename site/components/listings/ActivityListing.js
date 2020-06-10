2942b6c709d209d95d021e67516fd3257ce0c01b
Robin de Mourat <robin.demourat@gmail.com>
2020-06-10 19:11:42 +0200

    implement first version of research-streams in activities listing

--- site/components/listings/ActivityListing.js
+++ site/components/listings/ActivityListing.js
@@ -16,6 +16,10 @@ const messagesMeta = {
   description: {
     fr: 'Les activités du médialab s’articulent entre recherche et enseignement. Elles approfondissent notamment l’usage des méthodes numériques pour répondre aux enjeux contemporains en sociologie et STS. Également mobilisées au coeur d’activités pédagogiques, par exemple en mettant les étudiants en situation d’enquête, elles bénéficient d’une réflexivité très enrichissante sur nos processus de recherche.',
     en: 'The médialab’s activities articulate both research and teaching. In particular, they explore the use of digital methods to address contemporary issues in sociology and STS. These methods are also central to the laboratory’s teaching, for example by putting students in research situations. This fosters highly enriching reflection on our research processes.'
+  },
+  pinnedActivity: {
+    fr: 'Courant de recherche',
+    en: 'Research stream'
   }
 };
 
@@ -32,7 +36,9 @@ const sortActivities = (a, b) => {
 };
 
 export default function ActivityListing({lang, list, topActivities}) {
-  const activities = topActivities.map(ta => list.find(a => a.id === ta)).concat(list.sort(sortActivities).filter(a => topActivities.indexOf(a.id) === -1));
+  const activities = topActivities.map(ta => list.find(a => a.id === ta))
+  .map(ta => ({...ta, pinned: true}))
+  .concat(list.sort(sortActivities).filter(a => topActivities.indexOf(a.id) === -1));
 
   return (
     <>
@@ -54,25 +60,28 @@ export default function ActivityListing({lang, list, topActivities}) {
                 itemProp="member"
                 itemType={a.type === 'research' ? 'https://schema.org/ResearchProject' : 'https://schema.org/Project'}
                 data-type={a.type}
-                className={`list-item ${a.type}-${a.active ? 'active' : 'past'}`}>
+                className={`list-item ${a.type}-${a.active ? 'active' : 'past'} ${a.pinned ? 'pinned' : ''}`}>
                 <Link to={a.permalink[lang]}>
                   <div className="image-pre" aria-hidden="true">
                     <ProcessedImage size="medium" image={a.coverImage && a.coverImage.processed && a.coverImage.processed.medium} data={a} />
                   </div>
                   <div className="bandeau">
-                    <p data-icon="activite" className="type-activity">{I18N_TYPE_LABELS.activities[lang][a.type]}</p>
-                    <p className="title" itemProp="name" data-level-2="title">{a.name}</p>
+                    <p data-icon="activite" className="type-activity">{a.pinned ? messagesMeta.pinnedActivity[lang] : I18N_TYPE_LABELS.activities[lang][a.type]}</p>
+                    {!a.pinned && <p className="title" itemProp="name" data-level-2="title">{a.name}</p>}
                   </div>
                   <hgroup>
                     <h1 data-level-1="baseline" >
                       <LanguageFallback lang={lang} translatedAttribute={a.baseline} />
                     </h1>
                   </hgroup>
-                  <div className="accroche">
-                    <p className="accroche-paragraphe">
-                      <LanguageFallback lang={lang} translatedAttribute={a.description} />
-                    </p>
-                  </div>
+                  {
+                    !a.pinned &&
+                    <div className="accroche">
+                      <p className="accroche-paragraphe">
+                        <LanguageFallback lang={lang} translatedAttribute={a.description} />
+                      </p>
+                    </div>
+                  }
                 </Link>
               </li>
             ))}

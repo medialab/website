c85a1f609f57c3f40ada00af3b7701df8ffa54e7
Yomguithereal <guillaumeplique@gmail.com>
2020-07-08 14:32:31 +0200

    I18n integration for attachments Related to #507

--- site/components/details/fragments/Attachments.js
+++ site/components/details/fragments/Attachments.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
+import {translateAttachmentLabel} from '../../../../specs/translations';
+
 const i18n = {
   fr: {
-    attachment(file) {
-      return 'Lien vers la pièce jointe ' + file.value;
+    attachment(data) {
+      return 'Lien vers la pièce jointe ' + data.value;
     },
-    site(file) {
-      return 'Lien vers le site ' + file.value;
+    site(data) {
+      return 'Lien vers le site ' + data.value;
     }
   },
   en: {
-    attachment(file) {
-      return 'Link to attachment file ' + file.value;
+    attachment(data) {
+      return 'Link to attachment data ' + data.value;
     },
-    site(file) {
-      return 'Link to ' + file.value;
+    site(data) {
+      return 'Link to ' + data.value;
     }
   }
 };
@@ -25,36 +27,46 @@ const Attachments = ({lang, attachments}) => {
   if (!attachments || attachments.length === 0)
     return null;
 
+  attachments = attachments.filter(data => !data.lang || data.lang === lang);
+
   return (
     <ul className="fichiers-associes" id="attachments">
-      {(attachments || []).map((file, index) => (
-        <li key={file.value} data-type="files" className={`${index === 0 ? 'important' : ''} files${['attachement', 'url'].includes(file.type) ? ' target' : ''}`}>
+      {(attachments || []).map((data, index) => {
 
-          {file.type === 'attachment' && (
-            <a
-              href={file.value}
-              title={i18n[lang].attachment(file)}
-              aria-label={i18n[lang].attachment(file)}
-              target="_blank"
-              rel="noopener noreferrer">
-              {file.label}
-            </a>
-          )}
-          {file.type === 'url' && (
-            <a
-              title={i18n[lang].site(file)}
-              href={file.value}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={i18n[lang].site(file)}>
-              {file.label}
-            </a>
-          )}
-          {file.type === 'string' && (
-            <span>{file.label}: {file.value}</span>
-          )}
-        </li>
-      ))}
+        data = Object.assign({}, data);
+
+        if (!data.lang && lang === 'en')
+          data.label = translateAttachmentLabel(data.label);
+
+        return (
+          <li key={data.value} data-type="files" className={`${index === 0 ? 'important' : ''} files${['attachement', 'url'].includes(data.type) ? ' target' : ''}`}>
+
+            {data.type === 'attachment' && (
+              <a
+                href={data.value}
+                title={i18n[lang].attachment(data)}
+                aria-label={i18n[lang].attachment(data)}
+                target="_blank"
+                rel="noopener noreferrer">
+                {data.label}
+              </a>
+            )}
+            {data.type === 'url' && (
+              <a
+                title={i18n[lang].site(data)}
+                href={data.value}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={i18n[lang].site(data)}>
+                {data.label}
+              </a>
+            )}
+            {data.type === 'string' && (
+              <span>{data.label}: {data.value}</span>
+            )}
+          </li>
+        );
+      })}
     </ul>
   );
 };

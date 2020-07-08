1ea02e6328a30166e58415dbcf3036645170392a
Yomguithereal <guillaumeplique@gmail.com>
2020-07-08 10:51:54 +0200

    Better translations, related to #507

--- specs/translations.js
+++ specs/translations.js
@@ -7,7 +7,27 @@ const ATTACHMENT_LABELS = {
   'site internet': 'Website',
   'code source': 'Source code',
   'tutoriel': 'Tutorial',
-  'téléchargement': 'Download'
+  'téléchargement': 'Download',
+
+  // People
+  'mail': 'E-mail',
+  'site': 'Website',
+  'cv': 'Resume',
+  'site personnel': 'Personal website',
+
+  // News
+  's\'inscrire': 'Registration',
+  'article de presse': 'Press article',
+  'fiche de poste': 'Job description',
+  'cahier des charges': 'Specifications',
+  'rejoindre la conférence': 'Join the conference',
+  'texte de la motion': 'Motion text',
+  'appel à candidature': 'Call',
+  'assister aux présentations': 'Attend the talks',
+
+  // Productions/Activities
+  'blog du projet': 'Project blog',
+  'site du projet': 'Project website'
 };
 
 exports.translateAttachmentLabel = function translateAttachmentLabel(string) {

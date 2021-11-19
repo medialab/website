52aa761b3f4f6d7710bdac9f86a1ffa07cde8653
Yomguithereal <guillaumeplique@gmail.com>
2021-11-19 12:03:18 +0100

    Improving spire/hal join script

--- scripts/hal/dump-spire-docs.js
+++ scripts/hal/dump-spire-docs.js
@@ -1,35 +1,76 @@
+const csvWriter = require('csv-write-stream');
+
+const reducers = require('../../wilson/reducers');
 const HALClient = require('../../api/hal/client');
-const productions = require('../../data/productions.json');
+const DATA = require('../../data/productions.json');
 
-const existingSpireIds = new Set(
-  productions.productions
-    .filter(p => {
-      return p.spire && p.spire.id;
-    })
-    .map(p => p.spire.id)
-);
+const spireDocsInSite = new Map();
 
-console.error(`We have ${existingSpireIds.size} known spire ids.`);
+DATA.productions
+  .filter(p => p.spire && p.spire.id)
+  .map(p => reducers.productions('/', p))
+  .forEach(p => spireDocsInSite.set(p.spire.id, p));
 
-console.log('spire_id_in_hal,known');
+const spireIdsInHAL = new Set();
+
+console.error(`We have ${spireDocsInSite.size} known spire ids.`);
+
+const writer = csvWriter({
+  headers: ['spireId', 'inHAL', 'inSite', 'type', 'group', 'refSpire', 'refHAL']
+});
+writer.pipe(process.stdout);
 
 const client = new HALClient();
 
 let found = 0;
 
+function dumpDocsNotInHAL() {
+  spireDocsInSite.forEach((p, id) => {
+    if (spireIdsInHAL.has(id)) return;
+
+    writer.write({
+      spireId: id,
+      inHAL: 'no',
+      inSite: 'yes',
+      type: p.type,
+      group: p.group,
+      refSpire: p.ref.trim()
+    });
+  });
+}
+
 client.searchDocsWithSpireId(
   doc => {
     const docId = doc.sciencespoId_s;
+    spireIdsInHAL.add(docId);
 
-    if (existingSpireIds.has(docId)) {
+    const inSite = spireDocsInSite.get(docId);
+
+    if (inSite) {
       found++;
-      console.log(`${docId},yes`);
+      writer.write({
+        spireId: docId,
+        inHAL: 'yes',
+        inSite: 'yes',
+        type: inSite.type,
+        group: inSite.group,
+        refSpire: inSite.ref.trim(),
+        refHAL: doc.label_s.trim()
+      });
     } else {
-      console.log(`${docId},no`);
+      writer.write({
+        spireId: docId,
+        inHAL: 'yes',
+        inSite: 'no',
+        refSpire: doc.label_s.trim()
+      });
     }
   },
   err => {
     if (err) return console.error(err);
+
+    dumpDocsNotInHAL();
+    writer.end();
 
     console.error(`We found ${found} docs in HAL.`);
   }

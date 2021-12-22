2d486f2567dabf761b46b8c5674d9b8d8190f6da
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2021-12-22 11:15:04 +0100

    update directeur de publi + fix typo in mentions legales

--- site/components/Legal.js
+++ site/components/Legal.js
@@ -100,9 +100,9 @@ export default function Legal({lang}) {
 
             <h3 id="directeur">Directeur de la publication</h3>
             <p>
-              Bénédicte Durand, administratrice provisoire de l’Institut
-              d’études politiques de Paris et de la Fondation nationale des
-              sciences politiques.
+              Mathias Vicherat, Administrateur de la Fondation Nationale des
+              Sciences Politiques et Directeur de l’Institut d’Études
+              Politiques de Paris.
             </p>
 
             <h3 id="host">Hébergement</h3>
@@ -222,7 +222,7 @@ export default function Legal({lang}) {
 
             <h3 id="IL">Informatique et Libertés</h3>
             <p>
-              En vertu de l'article article 6.1.e du Règlement général européen
+              En vertu de l'article 6.1.e du Règlement général européen
               sur la protection des données 2016/679 (dit “RGPD”), les données
               personnelles traitées sur le site web sont, selon les finalités de
               traitement, nécessaires à l’exécution de la mission de service

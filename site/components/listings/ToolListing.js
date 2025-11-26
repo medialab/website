b4fa28e3dfa9c35795a3c74b280a76aff7c69775
Yomguithereal <guillaumeplique@gmail.com>
2025-11-26 14:05:19 +0100

    Basic separation of tools

--- site/components/listings/ToolListing.js
+++ site/components/listings/ToolListing.js
@@ -42,13 +42,77 @@ export default function ToolListing({lang, list}) {
   const otherLang = lang === 'fr' ? 'en' : 'fr';
   const joinText = lang === 'fr' ? ' et ' : ' and ';
 
+  const externalToolsSorted = list
+    .filter(tool => {
+      return tool.external;
+    })
+    .sort((a, b) => compare(b.date || '0', a.date || '0'));
+
   const toolsSorted = list
-    .slice()
-    .sort(
-      (a, b) =>
-        compare(!!a.external, !!b.external) ||
-        compare(b.date || '0', a.date || '0')
+    .filter(tool => {
+      return !tool.external;
+    })
+    .sort((a, b) => compare(b.date || '0', a.date || '0'));
+
+  function renderTool(tool) {
+    let usagesText;
+    let usagesClass;
+    if (tool.usages && tool.usages.length) {
+      usagesText = composeText(
+        tool.usages,
+        joinText,
+        I18N_TYPE_LABELS.toolsUsages[lang]
+      );
+      usagesClass = tool.usages.join(' ');
+    }
+    const dateYear = tool.date && getYear(parseISO(tool.date));
+
+    return (
+      <li
+        key={tool.id}
+        itemScope
+        itemType={productionTypeToSchemaURL(tool.type)}
+        data-item={nbItem}
+        data-type={tool.type}
+        className={`tool-portrait list-item ${tool.audience} ${tool.status} ${usagesClass}`}>
+        <Link to={tool.permalink[lang]}>
+          <figure>
+            {tool.coverImage ? (
+              <img itemProp="image" src={tool.coverImage.url} />
+            ) : (
+              <ImagePlaceholder
+                type="tool"
+                alt={tool.title[lang] || tool.title[otherLang]}
+              />
+            )}
+          </figure>
+
+          <div className="description">
+            <div className="header">
+              <h1 itemProp="name" data-level-1="title">
+                {tool.title[lang] || tool.title[otherLang]}
+              </h1>
+              <h2 itemProp="description">
+                {tool.description &&
+                  (tool.description[lang] || tool.description[otherLang])}
+              </h2>
+            </div>
+            <div className="details">
+              {tool.usages && (
+                <p className="detail-usages important">{usagesText}</p>
+              )}
+              <p className="detail-external">
+                {tool.external
+                  ? i18n[lang].externalTool
+                  : i18n[lang].internalTool}
+              </p>
+              <p className="detail-date">{dateYear}</p>
+            </div>
+          </div>
+        </Link>
+      </li>
     );
+  }
 
   return (
     <>
@@ -63,68 +127,10 @@ export default function ToolListing({lang, list}) {
 
         <section id="liste" className="main-container">
           <ul className="liste_objet list-grid-layout" id="liste-tools">
-            {toolsSorted.map((tool, index) => {
-              let usagesText;
-              let usagesClass;
-              if (tool.usages && tool.usages.length) {
-                usagesText = composeText(
-                  tool.usages,
-                  joinText,
-                  I18N_TYPE_LABELS.toolsUsages[lang]
-                );
-                usagesClass = tool.usages.join(' ');
-              }
-              const dateYear = tool.date && getYear(parseISO(tool.date));
-
-              return (
-                <li
-                  key={index}
-                  itemScope
-                  itemType={productionTypeToSchemaURL(tool.type)}
-                  data-item={nbItem}
-                  data-type={tool.type}
-                  className={`tool-portrait list-item ${tool.audience} ${tool.status} ${usagesClass}`}>
-                  <Link to={tool.permalink[lang]}>
-                    <figure>
-                      {tool.coverImage ? (
-                        <img itemProp="image" src={tool.coverImage.url} />
-                      ) : (
-                        <ImagePlaceholder
-                          type="tool"
-                          alt={tool.title[lang] || tool.title[otherLang]}
-                        />
-                      )}
-                    </figure>
-
-                    <div className="description">
-                      <div className="header">
-                        <h1 itemProp="name" data-level-1="title">
-                          {tool.title[lang] || tool.title[otherLang]}
-                        </h1>
-                        <h2 itemProp="description">
-                          {tool.description &&
-                            (tool.description[lang] ||
-                              tool.description[otherLang])}
-                        </h2>
-                      </div>
-                      <div className="details">
-                        {tool.usages && (
-                          <p className="detail-usages important">
-                            {usagesText}
-                          </p>
-                        )}
-                        <p className="detail-external">
-                          {tool.external
-                            ? i18n[lang].externalTool
-                            : i18n[lang].internalTool}
-                        </p>
-                        <p className="detail-date">{dateYear}</p>
-                      </div>
-                    </div>
-                  </Link>
-                </li>
-              );
-            })}
+            {toolsSorted.map(renderTool)}
+          </ul>
+          <ul className="liste_objet list-grid-layout" id="liste-tools">
+            {externalToolsSorted.map(renderTool)}
           </ul>
         </section>
       </main>
